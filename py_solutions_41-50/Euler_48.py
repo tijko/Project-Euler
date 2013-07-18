51fe53dfe188371db61e4aea2594d51dc7eda680
Timmy Konick <konick781@gmail.com>
2013-07-18 01:45:03 -0400

    py solutions_41-50

---
+++ py_solutions_41-50/Euler_48.py
@@ -0,0 +1,13 @@
+# The last ten-digits of the sum of series 1**1 + 2**2 + 3**3 ..... + 1000**1000 
+
+import timeit
+
+
+start = timeit.default_timer()
+
+def euler_48():
+    return str(sum([i**i for i in range(1,1001)]))[-10:]
+
+print "Answer: %s" % euler_48()
+stop = timeit.default_timer()
+print "Time: %f" % (stop - start)
---
+++ py_solutions_41-50/Euler_50.py
@@ -0,0 +1,35 @@
+# prime under a million that is the biggest of the sum of the most consecutive primes 
+
+import math
+import timeit
+
+
+start = timeit.default_timer()
+
+def is_prime(x):
+    if x == 2:
+        return True
+    if x == 1 or x % 2 == 0:
+        return False
+    for i in range(3, int(math.sqrt(x)) + 1, 2):
+        if x % i == 0:
+            return False
+    return True
+
+def euler_50():
+    high = 0
+    top = 0
+    primes = [i for i in xrange(3944) if is_prime(i)]
+    for prime in primes:
+        total = 0
+        for i in xrange(primes.index(prime), len(primes)):
+            total += primes[i]
+            if is_prime(total):
+                if high < primes.index(primes[i]) - primes.index(prime):
+                    high = primes.index(primes[i]) - primes.index(prime) 
+                    top = total
+    return top
+
+print "Answer: %s" % euler_50()
+stop = timeit.default_timer()
+print "Time: %f" % (stop - start)    
