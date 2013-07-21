487348ace806c96a24d2d90e4a5c85f8ad5e52a5
Timmy Konick <konick781@gmail.com>
2013-07-20 20:21:02 -0400

    rm .index

--- py_solutions_1-10/Euler_9.py
+++ py_solutions_1-10/Euler_9.py
@@ -8,13 +8,11 @@ start = timeit.default_timer()
 
 def euler_9():
     triplet_range = range(1, 3000)
-    b = 0
     for a in triplet_range:
-        b = triplet_range.index(a) + 1
+        b = a + 1
         while b < len(triplet_range):
             if a + b + (math.sqrt(a**2 + b**2)) == 1000:
-                return ("Answer = %s | Triplet: A=%s, B=%s, C=%s" %  
-                        (a * b * (math.sqrt(a**2 + b**2)), a, b, (math.sqrt(a**2 + b**2))))
+                return ("Answer: %s" % int((a * b * (math.sqrt(a**2 + b**2)))))
             if a + b + (math.sqrt(a**2 + b**2)) > 1000:
                 break
             b += 1
