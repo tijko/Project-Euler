48798257d53856571387e5df89d287ea3c33a78f
Timmy Konick <konick781@gmail.com>
2013-07-24 23:21:15 -0400

    even faster

--- py_solutions_11-20/Euler_12.py
+++ py_solutions_11-20/Euler_12.py
@@ -9,8 +9,8 @@ start = timeit.default_timer()
 def euler_12():
     num = 1001
     divisors = 2 
+    tri = sum(xrange(1, num + 1))
     while True:
-        tri = sum(xrange(1, num + 1))
         for i in xrange(2, int(math.sqrt(tri)) + 1):
             if tri % i == 0:
                 divisors += 2
@@ -19,7 +19,8 @@ def euler_12():
         else:
             divisors = 2
             num += 1
-    
+            tri += num
+
 print "Answer: %s" % euler_12()
 stop = timeit.default_timer()
 print ("Time: %f" % (stop - start))
