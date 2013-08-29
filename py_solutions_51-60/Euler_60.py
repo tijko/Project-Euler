e4dd8cf715be34febd473eaf0ca18a46d1965639
Timmy Konick <konick781@gmail.com>
2013-08-29 12:46:18 -0400

    faster recursive method

--- py_solutions_51-60/Euler_60.py
+++ py_solutions_51-60/Euler_60.py
@@ -1,4 +1,4 @@
-# Find lowest sum of a set of fives primes for which any two can be concatenated to form a prime in any order
+# Find lowest sum of set of five primes, that any two can be joined to form a prime in any order.
 
 import math
 import itertools
@@ -17,26 +17,32 @@ def is_prime(x):
             return False
     return True
 
-def prime_combos(*args):
-    for i in itertools.combinations(args, 2):
+def prime_combos(canidates):
+    for i in itertools.combinations(canidates, 2):
         if not is_prime(int(i[0] + i[1])) or not is_prime(int(i[1] + i[0])):
-            return False
+            return False    
     return True
 
-def euler_60():
-    prime = [w for w in xrange(7, 10000) if is_prime(w)] 
-    for i in prime:
-        for j in prime[prime.index(i):]:
-            if prime_combos(str(i), str(j)):
-                for v in prime[prime.index(j):]:
-                    if prime_combos(str(i), str(j), str(v)):
-                        for e in prime[prime.index(v):]:
-                            if prime_combos(str(i), str(j), str(v), str(e)):
-                                for x in prime[prime.index(e):]:
-                                    if prime_combos(str(i), str(j), str(v), str(e), str(x)):
-                                        return sum([i, j, v, e, x])
+def euler_60(primes=None, combos=None):
+    if not combos:
+        primes = [w for w in xrange(7, 10000) if is_prime(w)]
+        for i in primes:
+            combos = euler_60(primes[primes.index(i) + 1:], [str(i)])         
+            if len(combos) == 5:
+                return sum([int(i) for i in combos])
+    else:
+        for j in primes:
+            if prime_combos(combos + [str(j)]):
+                if len(combos + [str(j)]) == 5:
+                    combos.append(str(j))
+                    return combos
+                combos = euler_60(primes[primes.index(j) + 1:], 
+                                      combos + [str(j)])
+            if len(combos) == 5:
+                return combos
+        combos.pop(-1)
+        return combos 
 
-print "Answer: %s" % euler_60()
+print "Answer: %s" % euler_60()    
 stop = timeit.default_timer()
-print "Time: %s" % str(stop - start)
-
+print "Time: %f" % (stop - start)
