0907abd579271356649b29bba8f11ef88c668a7c
Timmy Konick <konick781@gmail.com>
2013-10-25 04:46:29 -0400

    split into separate functions

--- py_solutions_81-90/Euler_83.py
+++ py_solutions_81-90/Euler_83.py
@@ -22,10 +22,7 @@ with open(path + 'euler_txt/matrix.txt') as f:
 
 edges = [[int(i) for i in v.split(',')] for v in grid.split('\r\n') if v]      
 
-traveled = []
-for i in range(80): 
-    row = ['inf'] * 80
-    traveled.append(row)
+traveled = [['inf'] * 80 for i in xrange(80)]
 
 x = y = 0
 heap = [[y, x]]
@@ -40,37 +37,13 @@ def euler_83(y, x):
     if x + 1 >= bounds and y + 1 >= bounds: 
         return
     if y + 1 < bounds:
-        d_vertex = curr + edges[y+1][x]
-        if traveled[y+1][x] == 'inf':
-            traveled[y+1][x] = d_vertex
-        elif curr + edges[y+1][x] < traveled[y+1][x]:
-            traveled[y+1][x] = d_vertex 
-        else:
-            d_vertex = False
+        d_vertex = d_edge(y, x, curr)
     if x + 1 < bounds:
-        r_vertex = curr + edges[y][x+1]
-        if traveled[y][x+1] == 'inf':
-            traveled[y][x+1] = r_vertex
-        elif curr + edges[y][x+1] < traveled[y][x+1]:
-            traveled[y][x+1] = r_vertex
-        else:
-            r_vertex = False
+        r_vertex = r_edge(y, x, curr)
     if y - 1 >= 0:  
-        u_vertex = curr + edges[y-1][x]
-        if traveled[y-1][x] == 'inf':
-            traveled[y-1][x] = u_vertex
-        elif curr + edges[y-1][x] < traveled[y-1][x]:
-            traveled[y-1][x] = u_vertex
-        else:
-            u_vertex = False
+        u_vertex = u_edge(y, x, curr)
     if x - 1 >= 0:
-        l_vertex = curr + edges[y][x-1]
-        if traveled[y][x-1] == 'inf':
-            traveled[y][x-1] = l_vertex
-        elif curr + edges[y][x-1] < traveled[y][x-1]:
-            traveled[y][x-1] = l_vertex
-        else:
-            l_vertex = False
+        l_vertex = l_edge(y, x, curr)
     mvs = {d_vertex:'d_vertex',
            r_vertex:'r_vertex',
            u_vertex:'u_vertex',
@@ -79,41 +52,67 @@ def euler_83(y, x):
     if any(mvs):
         mvs = {k:v for k,v in mvs.items() if k}
         next_mv = min(mvs)
-        heap_mv = mvs.values()
+        heap_mv = [mv for mv in mvs.values() if mv != mvs[next_mv]]
+        push_heap(y, x, heap_mv)
         if mvs[next_mv] == 'd_vertex':
-            if 'r_vertex' in heap_mv:
-                heap.append([y,x+1])
-            if 'u_vertex' in heap_mv:
-                heap.append([y-1,x])
-            if 'l_vertex' in heap_mv:
-                heap.append([y,x-1])
             euler_83(y+1, x)
         elif mvs[next_mv] == 'r_vertex':
-            if 'd_vertex' in heap_mv:
-                heap.append([y+1,x])
-            if 'u_vertex' in heap_mv:
-                heap.append([y-1,x])
-            if 'l_vertex' in heap_mv:
-                heap.append([y,x-1])
             euler_83(y, x+1)
         elif mvs[next_mv] == 'u_vertex':
-            if 'd_vertex' in heap_mv:
-                heap.append([y+1,x])
-            if 'r_vertex' in heap_mv:
-                heap.append([y,x+1])
-            if 'l_vertex' in heap_mv:
-                heap.append([y,x-1])
             euler_83(y-1,x)
         else:
-            if 'd_vertex' in heap_mv:
-                heap.append([y+1,x])
-            if 'r_vertex' in heap_mv:
-                heap.append([y,x+1])
-            if 'u_vertex' in heap_mv:
-                heap.append([y-1,x])
             euler_83(y,x-1)
     else:
         return    
+
+def d_edge(y, x, curr):
+    d_vertex = curr + edges[y+1][x]
+    if traveled[y+1][x] == 'inf':
+        traveled[y+1][x] = d_vertex
+    elif d_vertex < traveled[y+1][x]:
+        traveled[y+1][x] = d_vertex 
+    else:
+        d_vertex = False
+    return d_vertex
+
+def r_edge(y, x, curr):
+    r_vertex = curr + edges[y][x+1]
+    if traveled[y][x+1] == 'inf':
+        traveled[y][x+1] = r_vertex
+    elif r_vertex < traveled[y][x+1]:
+        traveled[y][x+1] = r_vertex
+    else:
+        r_vertex = False
+    return r_vertex
+
+def u_edge(y, x, curr):
+    u_vertex = curr + edges[y-1][x]
+    if traveled[y-1][x] == 'inf':
+        traveled[y-1][x] = u_vertex
+    elif u_vertex < traveled[y-1][x]:
+        traveled[y-1][x] = u_vertex
+    else:
+        u_vertex = False
+    return u_vertex
+
+def l_edge(y, x, curr):
+    l_vertex = curr + edges[y][x-1]
+    if traveled[y][x-1] == 'inf':
+        traveled[y][x-1] = l_vertex
+    elif l_vertex < traveled[y][x-1]:
+        traveled[y][x-1] = l_vertex
+    else:
+        l_vertex = False
+    return l_vertex
+
+def push_heap(y, x, heap_mv):
+    mv_coor = {'d_vertex':[y+1,x],
+               'r_vertex':[y,x+1],
+               'u_vertex':[y-1,x],
+               'l_vertex':[y,x-1]
+              }
+    [heap.append(mv_coor[i]) for i in heap_mv]
+    return
 
 while heap:
     y, x = heap.pop(0)
