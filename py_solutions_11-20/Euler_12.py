a733a63b346aa939251bcbcbf56e620c8d68f205
Timmy Konick <konick781@gmail.com>
2013-11-14 07:15:51 -0500

    recursive ver

--- py_solutions_11-20/Euler_12.py
+++ py_solutions_11-20/Euler_12.py
@@ -2,25 +2,22 @@
 
 import math
 import timeit
+import sys
 
 
+sys.setrecursionlimit(15000)
 start = timeit.default_timer()
 
-def euler_12():
-    num = 1001
+def euler_12(num):
     divisors = 2 
     tri = sum(xrange(1, num + 1))
-    while True:
-        for i in xrange(2, int(math.sqrt(tri)) + 1):
-            if tri % i == 0:
-                divisors += 2
-        if divisors > 500:
-            return tri
-        else:
-            divisors = 2
-            num += 1
-            tri += num
+    for i in xrange(2, int(math.sqrt(tri)) + 1):
+        if tri % i == 0:
+            divisors += 2
+            if divisors > 500:
+                return tri
+    return euler_12(num + 1)
 
-print "Answer: %s" % euler_12()
+print "Answer: %s" % euler_12(1001)
 stop = timeit.default_timer()
 print ("Time: %f" % (stop - start))
