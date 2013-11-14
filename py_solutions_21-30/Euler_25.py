f195ebf639e4d2da7cd9cc737ec0cbcebb5c67b7
Timmy Konick <konick781@gmail.com>
2013-11-14 07:42:11 -0500

    recursion; faster

--- py_solutions_21-30/Euler_25.py
+++ py_solutions_21-30/Euler_25.py
@@ -1,23 +1,17 @@
 ## what is the first fibonacci number to have 1000 digits in it? ##
 
 import timeit
+import sys
 
 
+sys.setrecursionlimit(10000)
 start = timeit.default_timer()
 
-def euler_25():
-    a1 = 1
-    count = 2
-    b1 = 1
-    while True:
-        b2 = a1 + b1
-        a1 = b1
-        b1 = b2
-        count += 1
-        if len(str(b2)) >= 1000:
-            return count
+def euler_25(x, y):
+    if len(str(x)) >= 1000: return 0
+    return euler_25(x + (x+y), x + y) + 2
 
 
-print "Answer: %s" % euler_25()
+print "Answer: %s" % euler_25(1, 0)
 stop = timeit.default_timer()
 print "Time: %s" % str(stop - start)
