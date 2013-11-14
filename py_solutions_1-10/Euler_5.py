6b1afc0f33a1f47607f8af620cfeecbfd01490ee
Timmy Konick <konick781@gmail.com>
2013-11-14 06:45:20 -0500

    speed up with range and heuristic

--- py_solutions_1-10/Euler_5.py
+++ py_solutions_1-10/Euler_5.py
@@ -7,14 +7,17 @@ start = timeit.default_timer()
 
 def euler_5():
     count = 20
-    divs = [i for i in xrange(1, 20 + 1)]
+    divs = range(1, 21)
     while True:
-        for i in divs:
-            if count % i != 0:
-                count += 20
-                break
-            if i == 20:
-                return count
+        if count % 7 != 0 or count % 9 != 0 or count % 3 != 0:
+            count += 20
+        else:
+            for i in divs:
+                if count % i != 0:
+                    count += 20
+                    break
+                if i == 20:
+                    return count
 
 print "Answer: %s" % euler_5()
 stop = timeit.default_timer()
