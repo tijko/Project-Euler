1292c29fd879030fcd848587198b36fd7f3a2444
Timmy Konick <konick781@gmail.com>
2013-11-29 23:42:51 -0500

    faster function

--- py_solutions_41-50/Euler_42.py
+++ py_solutions_41-50/Euler_42.py
@@ -7,18 +7,13 @@ import os
 start = timeit.default_timer()
 
 def euler_42():
-    total = 0
     triangle_numbers = [int((i * 0.5) * (i + 1)) for i in xrange(1, 101)]
-    with open(os.path.abspath('').strip('solutions_41-50') + '/euler_txt/words1.txt') as f:
+    with open(os.path.abspath('').strip('py_solutions_41-50') + 
+                                        '/euler_txt/words1.txt') as f:
         words = f.read()
-    words = [i for i in words.split('"') if i != ',' and i != '']
-    for word in words:
-        wrd_value = 0
-        for i in word:
-            wrd_value += (ord(i) - 64)       
-        if wrd_value in triangle_numbers:
-            total += 1
-    return total    
+    words = [i for i in words.split('"') if i and i != ',']
+    return sum([1 for i in words if 
+               sum([ord(j) - 64 for j in i]) in triangle_numbers])
 
 print "Answer: %s" % euler_42()
 stop = timeit.default_timer()
