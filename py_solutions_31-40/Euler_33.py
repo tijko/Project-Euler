f2664aab3d18ce87ee4a0b5fa6fa0b12ea67f2ca
Timmy Konick <konick781@gmail.com>
2013-12-02 14:31:12 -0500

    comments

--- py_solutions_31-40/Euler_33.py
+++ py_solutions_31-40/Euler_33.py
@@ -1,8 +1,9 @@
-## trivial example = 30/50 = 3/5 ##
-## non-trivial example = 49/98 = 4/8 ##
+# trivial example = 30/50 = 3/5 
+# non-trivial example = 49/98 = 4/8 
 
-## There are 4 non-trivial types of these fractions < 1 and both numerator and denominator contain  two digits ##
-## find the the product of the four in its lowest common terms, what is the denominator? ##
+# There are 4 non-trivial types of these fractions < 1 and both numerator and 
+# denominator contain  two digits find the the product of the four in its 
+# lowest common terms, what is the denominator? 
 
 import timeit
 import itertools
