cfd44c12235da1b219b63156b0eb3c5a986a36a7
Timmy Konick <konick781@gmail.com>
2013-12-02 11:56:17 -0500

    no idea what i was thinking indexing >.<

--- py_solutions_31-40/Euler_32.py
+++ py_solutions_31-40/Euler_32.py
@@ -3,21 +3,18 @@
 ## So whats the combinations of products to equal 9 digits? ##
 
 import timeit
-import string
 
 
 start = timeit.default_timer()
 
 def euler_32():
-    x = xrange(2, 100)
-    y = xrange(100, 5000)
     pandigitals = set()
-    pandigital = {n for n in string.digits[1:]}
-    for k in range(len(x)):
-        for i in y:
-            digi_str = ''.join(map(str, [x[k] * i, x[k], i]))
+    pandigital = {'1', '2', '3', '4', '5', '6', '7', '8', '9'}
+    for x in xrange(2, 100):
+        for y in xrange(100, 5000):
+            digi_str = ''.join(map(str, [x * y, x, y]))
             if len(digi_str) == 9 and {j for j in digi_str} == pandigital:
-                pandigitals.add(x[k] * i)
+                pandigitals.add(x * y)
     return sum(pandigitals)
 
 print "Answer: %s" % euler_32()
