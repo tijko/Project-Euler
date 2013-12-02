73ac5e2f374605f29e828ea9fd5a3a0d56e6e4b8
Timmy Konick <konick781@gmail.com>
2013-12-02 13:25:43 -0500

    rm factors of 11

--- py_solutions_31-40/Euler_33.py
+++ py_solutions_31-40/Euler_33.py
@@ -16,11 +16,12 @@ def euler_33():
     denominators = list()
     product = lambda x, y: x * y
     fractions = [f for f in itertools.permutations(xrange(10, 100), 2) 
-                 if f[0] < f[1] and not any(i % 10 == 0 for i in f)]
+                 if f[0] < f[1] and not any(i % 10 == 0 or 
+                                            i / 10 == i % 10 for i in f)]
     for f in fractions:
         n_cpm = {d for d in str(f[0])}
         d_cpm = {d for d in str(f[1])}
-        if len(n_cpm) > 1 and len(d_cpm) > 1 and n_cpm.intersection(d_cpm):
+        if n_cpm.intersection(d_cpm):
             shared_d = list(n_cpm.intersection(d_cpm))
             new_n = str(f[0]).replace(shared_d[0], '')
             new_d = str(f[1]).replace(shared_d[0], '')          
