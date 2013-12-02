027456c82325f0694986ea350083ac93b82b8419
Timmy Konick <konick781@gmail.com>
2013-12-02 13:51:04 -0500

    lambda for string cast

--- py_solutions_31-40/Euler_33.py
+++ py_solutions_31-40/Euler_33.py
@@ -1,9 +1,8 @@
-# trivial example = 30/50 = 3/5 
-# non-trivial example = 49/98 = 4/8 
+## trivial example = 30/50 = 3/5 ##
+## non-trivial example = 49/98 = 4/8 ##
 
-# There are 4 non-trivial types of these fractions < 1 and both numerator and 
-# denominator contain two digits find the the product of the four in its lowest 
-# common terms, what is the denominator? 
+## There are 4 non-trivial types of these fractions < 1 and both numerator and denominator contain  two digits ##
+## find the the product of the four in its lowest common terms, what is the denominator? ##
 
 import timeit
 import itertools
@@ -15,16 +14,17 @@ def euler_33():
     numerators = list()
     denominators = list()
     product = lambda x, y: x * y
+    to_string = lambda x, y: str(x).replace(y, '')
     fractions = [f for f in itertools.permutations(xrange(10, 100), 2) 
-                 if f[0] < f[1] and not any(i % 10 == 0 or 
+                 if f[0] < f[1] and not any(i % 10 == 0 or
                                             i / 10 == i % 10 for i in f)]
     for f in fractions:
         n_cpm = {d for d in str(f[0])}
         d_cpm = {d for d in str(f[1])}
         if n_cpm.intersection(d_cpm):
             shared_d = list(n_cpm.intersection(d_cpm))
-            new_n = str(f[0]).replace(shared_d[0], '')
-            new_d = str(f[1]).replace(shared_d[0], '')          
+            new_n = to_string(f[0], shared_d[0])
+            new_d = to_string(f[1], shared_d[0])
             if float(new_n) / int(new_d) == float(f[0]) / f[1]:
                 numerators.append(f[0])
                 denominators.append(f[1])
