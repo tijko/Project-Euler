3562a914799cb38dc6e5fcf048d948575ff1c68f
Timmy Konick <konick781@gmail.com>
2013-12-04 21:18:43 -0500

    make a oneliner ;)

--- py_solutions_31-40/Euler_35.py
+++ py_solutions_31-40/Euler_35.py
@@ -1,7 +1,6 @@
 ## circular primes ##
 
 import timeit
-import collections
 import math
 
 
@@ -18,20 +17,10 @@ def is_prime(x):
     return True
 
 def euler_35():
-    total = 0  
-    primes = [i for i in range(2, 1000000) if is_prime(i) and 
-              not any([v for v in str(i) if int(v) % 2 == 0])]
-    primes.append(2)
-    for prime in primes:
-        circle = collections.deque([int(v) for v in str(prime)])
-        for i in range(len(circle)):
-            circle.rotate(1)
-            num_word = ''.join([str(v) for v in circle])
-            if not is_prime(int(num_word)):
-                break
-            if i == len(circle) - 1:
-                total += 1
-    return total
+    return sum([1 for prime in [j for j in map(str, xrange(2, 1000000)) 
+                if is_prime(int(j)) and not any([v for v in j if 
+                int(v) % 2 == 0])] if all(is_prime(int(''.join(prime[i:] + 
+                prime[:i]))) for i in xrange(1, len(prime)))]) + 1 # add one for two 
         
 print "Answer: %s" % euler_35()
 stop = timeit.default_timer()
