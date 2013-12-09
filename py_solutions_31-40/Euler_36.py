7b13bafd8cab5cbca86fa8bc2f07c4117453b270
Timmy Konick <konick781@gmail.com>
2013-12-08 23:36:53 -0500

    oneliner

--- py_solutions_31-40/Euler_36.py
+++ py_solutions_31-40/Euler_36.py
@@ -6,11 +6,7 @@ import timeit
 start = timeit.default_timer()
 
 def euler_36():
-	ans = sum([i for i in xrange(1,1000000) if str(i)[::-1] == str(i) and 
-               bin(i).strip('0b') == bin(i).strip('0b')[::-1] and 
-               bin(i).strip('0b')[0] != '0' and 
-               bin(i)[len(bin(i))-1:] != '0'])
-	return ans
+	return sum([i for i in xrange(1,1000000) if str(i)[::-1] == str(i) and bin(i).strip('0b') == bin(i).strip('0b')[::-1] and bin(i).strip('0b')[0] != '0' and bin(i)[len(bin(i))-1:] != '0'])
 
 print "Answer: %s" % euler_36()
 stop = timeit.default_timer()
