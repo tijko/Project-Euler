148e4d9fbb4d65a67238744ea899169812584256
Timmy Konick <konick781@gmail.com>
2013-12-18 18:31:52 -0500

    map int

--- py_solutions_11-20/Euler_16.py
+++ py_solutions_11-20/Euler_16.py
@@ -6,7 +6,7 @@ import timeit
 start = timeit.default_timer()
 
 def euler_16():
-    return sum([int(i) for i in str(2**1000)])
+    return sum(map(int, str(2**1000)))
 
 print "Answer: %s" % euler_16()
 stop = timeit.default_timer()
