8e1357153780b8a731f4026b4c000856b418837e
Timmy Konick <konick781@gmail.com>
2013-12-18 18:30:03 -0500

    bad var name new

--- py_solutions_11-20/Euler_15.py
+++ py_solutions_11-20/Euler_15.py
@@ -1,14 +1,13 @@
 # find all paths in 20 X 20 grid
 
-import math
+from math import factorial
 import timeit
 
 
 start = timeit.default_timer()
 
 def euler_15():
-    new = math.factorial(40)
-    return (new / (math.factorial(20) * math.factorial(20)))
+    return (factorial(40) / (factorial(20) * factorial(20)))
 
 print "Answer: %s" % euler_15()
 stop = timeit.default_timer()
