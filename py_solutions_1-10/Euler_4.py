9da330fb43cda1c0df68d0c391b2b7cc9a69f1e5
Timmy Konick <konick781@gmail.com>
2013-12-20 01:58:42 -0500

    leave as generator

--- py_solutions_1-10/Euler_4.py
+++ py_solutions_1-10/Euler_4.py
@@ -7,13 +7,12 @@ import timeit
 start = timeit.default_timer()
 
 def euler_4():
-    canidates = [i for i in xrange(100, 1000)]
-    canidates = [i for i in itertools.combinations(canidates, 2)]
+    canidates = itertools.combinations(xrange(100, 1000), 2)
     current_high = 0
     for canidate in canidates:
-        new = canidate[0] * canidate[1]
-        if str(new)[::-1] == str(new) and new > current_high:
-            current_high = new
+        pal_prod = canidate[0] * canidate[1]
+        if str(pal_prod)[::-1] == str(pal_prod) and pal_prod > current_high:
+            current_high = pal_prod
     return current_high
 
 print "Answer: %s" % euler_4()
