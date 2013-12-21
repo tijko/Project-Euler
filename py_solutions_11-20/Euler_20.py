958e65d6b70bd7c682119e2f9040c7bffb712788
Timmy Konick <konick781@gmail.com>
2013-12-21 01:03:46 -0500

    map it

--- py_solutions_11-20/Euler_20.py
+++ py_solutions_11-20/Euler_20.py
@@ -7,7 +7,7 @@ import timeit
 start = timeit.default_timer()
 
 def euler_20():
-    return sum([int(i) for i in str(math.factorial(100))])
+    return sum(map(int, str(math.factorial(100))))
 
 print "Answer: %s" % euler_20()
 stop = timeit.default_timer()
