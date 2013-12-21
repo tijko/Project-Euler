3886efe66542e845cfdb41dd6e928afcfd155867
Timmy Konick <konick781@gmail.com>
2013-12-21 00:59:17 -0500

    itertools and zip

--- py_solutions_11-20/Euler_19.py
+++ py_solutions_11-20/Euler_19.py
@@ -1,16 +1,15 @@
 # find the number sundays that fell on the first day of the month in between 1901 - 2000 
 
 import timeit
+from itertools import cycle
 
 
 start = timeit.default_timer()
 
 def euler_19():
-    years = xrange(1901, 2000) 
     answer = 0 
-    weekday = 0  
-    day_dict = {}
-    for i in years: 
+    weekday = cycle(xrange(7))
+    for i in xrange(1901, 2000):
         if i % 4 == 0: 
             Feb = xrange(1, 30) 
         else: 
@@ -19,19 +18,10 @@ def euler_19():
         Apr = Jun = Sep = Nov = xrange(1, 31) 
         year = [Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec] 
         week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'] 
-        count = 1 
-        mos = 0 
-        while mos < len(year): 
-            for i in year[mos]: 
-                day_dict[count] = week[weekday] 
-                count += 1 
-                weekday += 1 
-                if weekday > 6: 
-                    weekday = 0 
-            if day_dict[1] == 'Sunday': 
-                answer += 1 
-            mos += 1 
-            count = 1 
+        for mos in xrange(12):
+            month = zip(year[mos], weekday)
+            if month[0] == (1, 6):
+                answer += 1            
     return answer 
 
 print "Answer: %s" % euler_19()
