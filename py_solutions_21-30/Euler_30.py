38cef9afb24e524e6c518dd8075c1066f45ba393
Timmy Konick <konick781@gmail.com>
2013-12-24 00:30:18 -0500

    map with a lambda

--- py_solutions_21-30/Euler_30.py
+++ py_solutions_21-30/Euler_30.py
@@ -6,17 +6,13 @@ import timeit
 start = timeit.default_timer()
 
 def euler_30():
-    canidates = (i for i in xrange(2, 1000000))
+    fifth_pow = lambda x: x**5
     total = 0
-    while True:
-        try:
-            i = canidates.next()
-            if sum(int(v)**5 for v in str(i)) == i:
-                total += i
-        except StopIteration:
-            return total
-
+    for i in xrange(2, 1000000):
+        if sum(map(fifth_pow, map(int, str(i)))) == i:
+            total += i
+    return total
 
 print "Answer: %s" % euler_30()
 stop = timeit.default_timer()
-print "Time: %s" % str(stop - start)
+print "Time: %f" % (stop - start)
