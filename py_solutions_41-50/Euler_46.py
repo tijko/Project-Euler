ee5ac126a694842cfc0320ac37896122f55ed845
Timmy Konick <konick781@gmail.com>
2013-12-25 16:26:49 -0500

    fixed errors

--- py_solutions_41-50/Euler_46.py
+++ py_solutions_41-50/Euler_46.py
@@ -19,19 +19,20 @@ def is_prime(x):
 def euler_46():   
     composite = 10
     while True:
-        while composite % 2 == 0 and is_prime(composite):
+        while composite % 2 == 0 or is_prime(composite):
             composite += 1
-        total = [2 * v**2 for v in xrange(int(math.sqrt(composite)) // 2)]
-        primes = (k for k in xrange(composite + 1) if is_prime(k))
+        total = [2 * v**2 for v in xrange(int(math.sqrt(composite)))]
+        primes = [k for k in xrange(composite + 1) if is_prime(k)]
         for v in total:
             for j in primes:
-                odds = total[v] + j 
+                odds = v + j 
                 if odds == composite: 
                     break 
             if odds == composite: 
                 break 
         if odds != composite:
             return composite
+        composite += 1
 
 print "Answer: %s" % euler_46()
 stop = timeit.default_timer()
