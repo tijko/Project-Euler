cd2bdf95eec1fc51702ec0ea9ac289497f532dec
Timmy Konick <konick781@gmail.com>
2013-12-27 01:49:53 -0500

    split into separate funcs. so search starts at smaller integer

--- py_solutions_41-50/Euler_47.py
+++ py_solutions_41-50/Euler_47.py
@@ -1,7 +1,7 @@
-# find the first of the four consecutive integer to have four distinct prime factors. 
+# Find the first of the four consecutive integers to have four distinct prime factors.
 
-import timeit
 import math
+import timeit
 
 
 start = timeit.default_timer()
@@ -15,19 +15,45 @@ def is_prime(x):
         if x % i == 0:
             return False
     return True
-    
+
+def comp_gen():
+    composite = 10 
+    while True:
+        while is_prime(composite):
+            composite += 1
+        yield composite
+        composite += 1
+ 
+def prime_gen(comp, p):
+    comp_range = (i for i in p if comp % i == 0)
+    while comp_range:
+        yield comp_range.next()
+
+def four_factors(comp, p_gen):
+    factors = 0
+    while factors < 4:
+        try:
+            if comp % p_gen.next() == 0:
+                factors += 1
+        except StopIteration:
+            return False
+    return True
 
 def euler_47():
-    composites = [i for i in xrange(10000, 265000) if not is_prime(i)]
-    primes = [i for i in xrange(2, 1000) if is_prime(i)]
-    four_factors = list()
-    for composite in composites:
-        if len([prime for prime in primes if composite % prime == 0]) >= 4:
-            four_factors.append(composite)
-    for i in xrange(len(four_factors)):
-        if four_factors[i:i+4] == range(four_factors[i], four_factors[i]+4):
-            return four_factors[i]
+    consecutive = 0
+    prev_comp = 0
+    comp = comp_gen()
+    primes = [i for i in xrange(2, 5000) if is_prime(i)]
+    while consecutive < 4:
+        cur_comp = comp.next()
+        p_gen = prime_gen(cur_comp, primes)
+        if four_factors(cur_comp, p_gen) and cur_comp - 1 == prev_comp:
+            consecutive += 1
+        else:
+            consecutive = 0
+        prev_comp = cur_comp
+    return cur_comp - 3                                     
 
-print "Answer: %s" % euler_47()
+print "Answer: %d" % euler_47()
 stop = timeit.default_timer()
 print "Time: %f" % (stop - start)
