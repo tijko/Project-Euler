82d57226b0f5deacc43fa037414e5b9874b5be78
Timmy Konick <konick781@gmail.com>
2013-12-29 01:19:00 -0500

    split into smaller functions

--- py_solutions_51-60/Euler_51.py
+++ py_solutions_51-60/Euler_51.py
@@ -1,6 +1,6 @@
-## smallest prime for which replacing part of the number you can form 8 different primes ##
-## Example: 189-00-37, 189-33-37, 189-44-37, 189-55-37, 189-66-37, 189-77-37, 189-88-37, 189-99-37 ##
-## the replacement digits do not have to be adjacent or consecutive but, have to be the same ##
+# smallest prime for which replacing part of the number you can form 8 different primes 
+# Example: 189-00-37, 189-33-37, 189-44-37, 189-55-37, 189-66-37, 189-77-37, 189-88-37, 189-99-37 
+# the replacement digits do not have to be adjacent or consecutive but, have to be the same 
 
 import timeit
 import math
@@ -18,22 +18,27 @@ def is_prime(x):
             return False
     return True
 
+def prime_gen():
+    replace_number = 10000
+    repeats = lambda x: any(x.count(v) > 2 for v in x)
+    while True:
+        if is_prime(replace_number) and repeats(str(replace_number)): 
+            yield str(replace_number)
+        replace_number += 1    
+
+def prime_replace(prime):
+    for i in prime:
+        if sum([1 for v in xrange(1, 10) if 
+                is_prime(int(prime.replace(i, str(v))))]) > 7:
+            return True
+    return False
+        
 def euler_51():
-    primes = list() 
-    for i in xrange(10000, 1000000):
-        if is_prime(i):
-            for v in str(i):
-                if str(i).count(v) > 2:
-                    primes.append(str(i))
-    for prime in primes:
-        for i in prime:
-            if prime.count(i) > 2:
-                count = 0
-                for v in xrange(1, 10):            
-                    if is_prime(int(prime.replace(i, str(v)))):
-                        count += 1
-                if count > 7:
-                    return prime
+    primes = prime_gen()
+    while True:
+        prime = primes.next()
+        if any(prime.count(i) > 2 for i in prime) and prime_replace(prime):
+            return prime
 
 print "Answer: %s" % euler_51()
 stop = timeit.default_timer()
