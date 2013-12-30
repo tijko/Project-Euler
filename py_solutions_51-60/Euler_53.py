e2811bc6b372f9ee1b6db5ca6b77c20fa9ba9137
Timmy Konick <konick781@gmail.com>
2013-12-30 02:14:27 -0500

    oneliner

--- py_solutions_51-60/Euler_53.py
+++ py_solutions_51-60/Euler_53.py
@@ -2,22 +2,15 @@
 
 '''How many, not necessarily distinct, values of  nCr, for 1 ≤ n ≤ 100, are greater than one-million?'''
 
-import math
+from math import factorial as fac
 import timeit
 
 
 start = timeit.default_timer()
 
 def euler_53():
-    values = [i for i in xrange(1, 101)]
-    count = 0
-    for i in values:
-        r = xrange(1, i)
-        for v in r:
-            if math.factorial(i) / (math.factorial(v) * math.factorial(i - v)) > 1000000:
-                count += 1
-    return count
+    return sum([1 for i in [l for v in zip([1] * 100, xrange(1, 101)) for l in xrange(v[1]) if fac(v[1]) / (fac(l) * fac(v[1] - l)) > 1000000]])
 
 print "Answer: %s" % euler_53()
 stop = timeit.default_timer()
-print "Time: %s" % str(stop - start)
+print "Time: %f" % (stop - start)
