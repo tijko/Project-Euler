bdc7aacf902a5eedb05d49ffd727cee4d60fa2cb
Timmy Konick <konick781@gmail.com>
2014-01-09 02:39:19 -0500

    rewrite

--- py_solutions_51-60/Euler_59.py
+++ py_solutions_51-60/Euler_59.py
@@ -8,43 +8,31 @@ import os
 start = timeit.default_timer()
 
 def euler_59():
-    frequent_wrds = ['the ', 'of ', 'and ', 'a ', 'to ', 'in ', 'is ', 'you ', 'that ', 'it ']
+    frequent_wrds = {'the', 'of', 'and', 'a', 'to', 'in', 'is', 'you', 'that', 'it'}
     '''
         "frequent_wrds" is a list of the top ten most frequently
         used words in english.  According to --->
         http://duboislc.org/EducationWatch/First100Words.html
     '''
-    with open(os.path.abspath('').strip('solutions_51-60') + '/euler_txt/cipher1.txt') as f:
-        encrypted_message = f.read() 
-    keys = [i for i in xrange(97, 123)]
-    keys = [i for i in itertools.combinations(keys, 3)] 
-    message = [int(i.strip('\r\n')) for i in encrypted_message.split(',')]
+    strip = lambda s: s.strip('\r\n')
+    alpha = lambda a: chr(int(a, 2))
+    binry = lambda b: b[2:][::-1]
+    with open(os.path.abspath('').strip('py_solutions_51-60') + 
+              '/euler_txt/cipher1.txt') as f:
+        encrypted_message = map(strip, f.read().split(',')) 
+    keys = itertools.combinations(xrange(97, 123), 3)
+    message = map(int, encrypted_message)
     for key in keys:
-        for i in [v for v in itertools.permutations(key)]:
-            msg = list()
-            kyd = 0 
-            for x in message:
-                cip = ''
-                x = bin(x) 
-                y = bin(i[kyd]) 
-                x = x[2:][::-1] 
-                y = y[2:][::-1] 
-                for j in xrange(len(x)):
-                    if y[j] == x[j]: 
-                        cip += '0' 
-                    if y[j] != x[j]: 
-                        cip += '1' 
-                cip += y[len(x):]
-                cip = cip[::-1] 
-                cip = chr(int(cip, 2)) 
-                msg.append(cip)
-                kyd += 1
-                if kyd == 3:
-                    kyd = 0 
-            if len([i for i in frequent_wrds if i in ''.join(msg)]) > 5:
-                print sum([ord(i) for i in ''.join(msg)])
-                return ''.join(msg) + '\n'
+        for i in itertools.permutations(key):
+            msg = '' 
+            kyd = itertools.cycle(map(bin, i)) 
+            for x in map(binry, map(bin, message)):
+                y = binry(kyd.next())
+                msg += alpha((''.join('0' if x[j] == y[j] else '1' for 
+                              j in xrange(len(x))) + y[len(x):])[::-1])
+            if len(frequent_wrds.intersection(msg.split())) > 5:                
+                return ''.join(msg) 
 
 print "Answer: %s" % euler_59()
 stop = timeit.default_timer()
-print "Time: %s" % str(stop - start)
+print "Time: %f" % (stop - start)
