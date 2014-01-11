648531fdf26213b8a31d7055bb2e8fe78ba1d225
Timmy Konick <konick781@gmail.com>
2014-01-10 23:24:37 -0500

    with filter or reduce

--- py_solutions_1-10/Euler_1.py
+++ py_solutions_1-10/Euler_1.py
@@ -8,6 +8,16 @@ start = timeit.default_timer()
 def euler_1():
     return sum([i for i in xrange(1, 1000) if i % 3 == 0 or i % 5 == 0])
 
+# with reduce
+#def euler_1():
+#    add = lambda x, y: x + y
+#    return reduce(add, [i for i in xrange(1000) if i % 3 == 0 or i % 5 == 0])
+
+# with filter
+#def euler_1():
+#    three_or_five = lambda x: x % 3 == 0 or x % 5 == 0
+#    return sum(filter(three_or_five, xrange(1000)))
+
 print "Answer: %s" % euler_1()
 stop = timeit.default_timer()
 print "Time: %f" % (stop - start)
