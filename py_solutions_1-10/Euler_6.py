75f15d75e64393a69fb5861b9096f8adadcdb93b
Timmy Konick <konick781@gmail.com>
2014-01-12 22:51:38 -0500

    no need to create two lists

--- py_solutions_1-10/Euler_6.py
+++ py_solutions_1-10/Euler_6.py
@@ -6,7 +6,7 @@ import timeit
 start = timeit.default_timer()
 
 def euler_6():
-    return sum([i for i in xrange(1, 101)])**2 - sum([i**2 for i in range(1, 101)])
+    return sum(xrange(1, 101))**2 - sum(map(lambda x: x**2, xrange(1, 101)))
 
 print "Answer: %s" % euler_6()
 stop = timeit.default_timer()
