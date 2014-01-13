701803c96a7b70eb4686cee7049dd307fa1a4386
Timmy Konick <konick781@gmail.com>
2014-01-12 22:44:41 -0500

    functools.partial

--- py_solutions_1-10/Euler_5.py
+++ py_solutions_1-10/Euler_5.py
@@ -1,23 +1,22 @@
-# what is the smallest number that can be divided evenly by all the numbers from 1-20 ?  
+# What is the smallest number that can be divided evenly by all the numbers from 1-20?  
 
 import timeit
+
+from functools import partial
+from operator import mod 
 
 
 start = timeit.default_timer()
 
 def euler_5():
-    count = 20
-    divs = range(1, 21)
+    dividend = 20
+    divisors = xrange(1, 21)
     while True:
-        if count % 7 != 0 or count % 9 != 0 or count % 3 != 0:
-            count += 20
+        if (dividend % 7 != 0 or dividend % 9 != 0 or dividend % 3 != 0 or
+            sum(map(partial(mod, dividend), divisors)) != 0):
+            dividend += 20
         else:
-            for i in divs:
-                if count % i != 0:
-                    count += 20
-                    break
-                if i == 20:
-                    return count
+            return dividend
 
 print "Answer: %s" % euler_5()
 stop = timeit.default_timer()
