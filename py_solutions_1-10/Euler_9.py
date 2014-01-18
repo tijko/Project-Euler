cb4a4c0db7fa69aa381b754dfc4bce01e34f585b
Timmy Konick <konick781@gmail.com>
2014-01-18 03:34:32 -0500

    sum cleaner

--- py_solutions_1-10/Euler_9.py
+++ py_solutions_1-10/Euler_9.py
@@ -1,4 +1,4 @@
-# find the only pythagorean triplet that a + b + c = 1000 
+# Find the only pythagorean triplet that a + b + c = 1000 
 
 import math
 import timeit
@@ -7,16 +7,16 @@ import timeit
 start = timeit.default_timer()
 
 def euler_9():
-    triplet_range = range(1, 3000)
+    triplet_range = xrange(1, 3000)
     for a in triplet_range:
         b = a + 1
         while b < len(triplet_range):
-            if a + b + (math.sqrt(a**2 + b**2)) == 1000:
+            if sum([a, b, (math.sqrt(a**2 + b**2))]) == 1000:
                 return ("Answer: %s" % int((a * b * (math.sqrt(a**2 + b**2)))))
-            if a + b + (math.sqrt(a**2 + b**2)) > 1000:
+            elif sum([a, b, (math.sqrt(a**2 + b**2))]) > 1000:
                 break
             b += 1
 
-print euler_9()
+print 'Answer: %s' % euler_9()
 stop = timeit.default_timer()
 print 'Time: %f' % (stop - start)
