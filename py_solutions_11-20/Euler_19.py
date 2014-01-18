c67053e3e69096e12cebcd991cae87577604a12d
Timmy Konick <konick781@gmail.com>
2014-01-18 05:09:36 -0500

    list comp with sum

--- py_solutions_11-20/Euler_19.py
+++ py_solutions_11-20/Euler_19.py
@@ -7,7 +7,7 @@ from itertools import cycle
 start = timeit.default_timer()
 
 def euler_19():
-    answer = 0 
+    sundays = 0 
     weekday = cycle(xrange(7))
     for i in xrange(1901, 2000):
         if i % 4 == 0: 
@@ -17,12 +17,11 @@ def euler_19():
         Jan = Mar = May = Jul = Aug = Oct = Dec = xrange(1, 32)    
         Apr = Jun = Sep = Nov = xrange(1, 31) 
         year = [Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec] 
-        week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'] 
-        for mos in xrange(12):
-            month = zip(year[mos], weekday)
-            if month[0] == (1, 6):
-                answer += 1            
-    return answer 
+        week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 
+                'Friday', 'Saturday', 'Sunday'] 
+        sundays += sum([1 for mos in xrange(12) if 
+                        zip(year[mos], weekday)[0] == (1, 6)]) 
+    return sundays 
 
 print "Answer: %s" % euler_19()
 stop = timeit.default_timer()
