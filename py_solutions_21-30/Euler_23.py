0e7b053008f3887f7d9cc3a58ca8a455869ef3a8
Timmy Konick <konick781@gmail.com>
2014-01-20 08:38:40 -0500

    itertools.ifilterfalse with a generator instead of list

--- py_solutions_21-30/Euler_23.py
+++ py_solutions_21-30/Euler_23.py
@@ -1,4 +1,4 @@
-## Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers. ##
+# Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers. 
 
 import timeit
 import itertools
@@ -6,14 +6,12 @@ import itertools
 
 start = timeit.default_timer()
 
-def euler_23():
-    abundant = list()
-    for i in xrange(1, 28124):
-        if i < sum([v for v in xrange(1, (i/2) + 1) if i % v == 0]):
-            abundant.append(i)
-    abundant_sums = set([i for i in combo_gen(abundant)]) 
-    return sum(set(xrange(1, 28124)).difference(abundant_sums))
 
+def euler_23():
+    abundant = (i for i in xrange(28124) if i < 
+                sum(itertools.ifilterfalse(lambda x, i=i: i % x, xrange(1, i / 2 + 1))))
+    abundant_sums = {i for i in combo_gen(abundant)}
+    return sum(abundant_sums.symmetric_difference(xrange(28124)))
 
 def combo_gen(x):
      for i in itertools.combinations_with_replacement(x, 2):
@@ -23,4 +21,4 @@ def combo_gen(x):
 
 print "Answer: %s" % euler_23()
 stop = timeit.default_timer()
-print "Time: %s" % str(stop - start)
+print "Time: %f" % (stop - start)
