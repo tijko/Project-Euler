774dcc5aac58d7e36f5f0ef67ab5722f291e195f
Timmy Konick <konick781@gmail.com>
2014-01-20 07:56:08 -0500

    map ord with lambda

--- py_solutions_21-30/Euler_22.py
+++ py_solutions_21-30/Euler_22.py
@@ -1,4 +1,4 @@
-# number amount of names
+# Number amount of names
 
 import timeit
 import os
@@ -12,10 +12,9 @@ def euler_22():
         name_list = f.read()
     name_list = name_list.replace('"', '')
     name_list = sorted(name_list.split(','))
-    return sum([(v + 1) * sum([ord(i) - 64 for i in k]) 
-                for v,k in enumerate(name_list)])
-
+    return sum([(v + 1) * sum(map(lambda x: ord(x) - 64, k)) 
+                for v, k in enumerate(name_list)])
 print "Answer: %s" % euler_22()
 stop = timeit.default_timer()
-print "Time: %s" % str(stop - start)
+print "Time: %f" % (stop - start)
 
