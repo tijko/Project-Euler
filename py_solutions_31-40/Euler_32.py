13eedb9edf7c2301747cecea486570a877acc82c
Timmy Konick <konick781@gmail.com>
2014-01-24 07:44:52 -0500

    itertools.product with nested loop

--- py_solutions_31-40/Euler_32.py
+++ py_solutions_31-40/Euler_32.py
@@ -1,8 +1,8 @@
-## Pandigital number 987654321 --- say 39 * 186 = 7254 all ##
-
-## So whats the combinations of products to equal 9 digits? ##
+# Pandigital number 987654321 --- say 39 * 186 = 7254 all 
+# So whats the combinations of products to equal 9 digits?
 
 import timeit
+import itertools
 
 
 start = timeit.default_timer()
@@ -10,11 +10,10 @@ start = timeit.default_timer()
 def euler_32():
     pandigitals = set()
     pandigital = {'1', '2', '3', '4', '5', '6', '7', '8', '9'}
-    for x in xrange(2, 100):
-        for y in xrange(100, 5000):
-            digi_str = ''.join(map(str, [x * y, x, y]))
-            if len(digi_str) == 9 and {j for j in digi_str} == pandigital:
-                pandigitals.add(x * y)
+    for x,y in itertools.product(xrange(2, 100), xrange(100, 5000)):
+        digi_str = ''.join(map(str, [x * y, x, y]))
+        if len(digi_str) == 9 and {j for j in digi_str} == pandigital:
+            pandigitals.add(x * y)
     return sum(pandigitals)
 
 print "Answer: %s" % euler_32()
