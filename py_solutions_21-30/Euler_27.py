27c299218da553d0b86913f6999250a3511b9433
Timmy Konick <konick781@gmail.com>
2014-01-23 22:21:00 -0500

    itertools for nested loop

--- py_solutions_21-30/Euler_27.py
+++ py_solutions_21-30/Euler_27.py
@@ -3,6 +3,7 @@
 
 import math
 import timeit
+import itertools
 
 
 start = timeit.default_timer()
@@ -19,20 +20,18 @@ def is_prime(x):
 
 
 def euler_27():
-    high = 0
-    for i in xrange(-999, 1000):
-        for v in xrange(-999, 1000):
-            count = 0
-            n = 0
-            quad = n**2 + (n*v) + i
-            while is_prime(abs(quad)):
-                count += 1
-                n += 1
-                quad = n**2 + (n*v) + i
-            if count > high:
-                high = count
-                total =  i*v
-    return total
+    max_primes = 0
+    quadratic = lambda x, y, z: x**2 + (x * y) + z
+    for i, v in itertools.product(xrange(-999, 1000), xrange(-999, 1000)):
+        primes = 0
+        q = quadratic(primes, v, i) 
+        while is_prime(abs(q)):
+            primes += 1
+            q = quadratic(primes, v, i)
+        if primes > max_primes:
+            max_primes = primes
+            co_prod =  i * v
+    return co_prod
 
 
 print "Answer: %s" % euler_27()
