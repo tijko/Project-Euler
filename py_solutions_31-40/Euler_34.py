36e541fd486025c697bb00418aacf303aaa40994
Timmy Konick <konick781@gmail.com>
2014-01-25 14:56:43 -0500

    slightly faster

--- py_solutions_31-40/Euler_34.py
+++ py_solutions_31-40/Euler_34.py
@@ -3,17 +3,14 @@
 ## Say 145 = 1! + 4! + 5! = 1 + 24 + 120 ##
 
 import timeit
-import math
+from math import factorial
 
 
 start = timeit.default_timer()
 
 def euler_34():
-    answers = []
-    for i in xrange(3, 7 * math.factorial(9)):
-        if sum([math.factorial(int(v)) for v in str(i)]) == i:
-            answers.append(i)
-    return sum(answers)
+    return sum([i for i in xrange(3, 7 * factorial(9)) if 
+                sum(map(factorial, map(int, str(i)))) == i])
 
 
 print "Answer: %s" % euler_34()
