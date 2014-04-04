bd10d6143708766d3de9084f971111ed7ccb5f6e
Tim Konick <konick781@gmail.com>
2014-04-03 23:53:44 -0400

    no need for while loop

--- py_solutions_1-10/Euler_9.py
+++ py_solutions_1-10/Euler_9.py
@@ -10,12 +10,11 @@ def euler_9():
     triplet_range = xrange(1, 3000)
     for a in triplet_range:
         b = a + 1
-        while b < len(triplet_range):
+        for b in xrange(b, 3000):
             if sum([a, b, (math.sqrt(a**2 + b**2))]) == 1000:
                 return ("Answer: %s" % int((a * b * (math.sqrt(a**2 + b**2)))))
             elif sum([a, b, (math.sqrt(a**2 + b**2))]) > 1000:
                 break
-            b += 1
 
 print 'Answer: %s' % euler_9()
 stop = timeit.default_timer()
