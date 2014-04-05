00a8104517b0272e645adf0606721bf337e4f5a3
Tim Konick <konick781@gmail.com>
2014-04-04 21:48:34 -0400

    fixed formatting

--- perl_solutions_1-10/Euler_6.pl
+++ perl_solutions_1-10/Euler_6.pl
@@ -1,15 +1,15 @@
 use strict;
 use warnings;
+use Time::HiRes qw( clock );
+
 
 sub diff_sum_sq
 {
-    my ($low, $high);
-    ($low, $high) = @_;
+    my ($low, $high) = @_;
 
     my $sum_sq;
     my $sq_sum;
-    foreach my $i ($low..$high)
-    {
+    foreach my $i ($low..$high) {
         $sum_sq += $i**2;
         $sq_sum += $i;
     }
@@ -17,5 +17,8 @@ sub diff_sum_sq
     return $sq_sum - $sum_sq;
 }
 
+my $start = clock();
 my $answer = diff_sum_sq(1, 100);
+my $stop = clock();
 print "Answer: ", $answer, "\n";
+printf "Time: %.5f\n", $stop - $start;
