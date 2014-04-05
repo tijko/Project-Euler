38f2eae7351cab949580f85a50f00f4ccc90b9db
Tim Konick <konick781@gmail.com>
2014-04-04 21:02:48 -0400

    clean up function

--- perl_solutions_1-10/Euler_2.pl
+++ perl_solutions_1-10/Euler_2.pl
@@ -1,22 +1,22 @@
 use strict;
 use warnings;
+use Time::HiRes qw( clock );
 
 
 sub fibonacci
 {
-    my ($t, $n, $ln, $limit);
-    ($t, $n, $ln, $limit) = @_;
-
-    if ($n >= $limit)
-    {
+    my ($t, $n, $ln, $limit) = @_;
+    if ($n >= $limit) {
         return $t;
     }
-    elsif ($n % 2 == 0)
-    {
+    elsif ($n % 2 == 0) {
         return fibonacci($t + $n, $n + $ln, $n, $limit);
     }
     return fibonacci($t, $n + $ln, $n, $limit);
 }
 
+my $start = clock();
 my $answer = fibonacci(0, 2, 1, 4000000);
+my $stop = clock();
 print "Answer: ", $answer, "\n";
+printf "Time: %.5f\n", $stop - $start;
