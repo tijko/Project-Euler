01cd097fbe2ab1d299841b015435fda338a05712
Tim Konick <konick781@gmail.com>
2014-04-04 22:18:37 -0400

    fixed formatting

--- perl_solutions_1-10/Euler_10.pl
+++ perl_solutions_1-10/Euler_10.pl
@@ -1,25 +1,20 @@
 use strict;
 use warnings;
 use Math::Complex;
+use Time::HiRes qw( clock );
 
 
 sub is_prime
 {
-    my $n;
-    ($n) = @_;
-
-    if ($n == 2)
-    {
+    my ($n) = @_;
+    if ($n == 2) {
         return 1;
     }
-    if ($n % 2 == 0 || $n == 1)
-    {
+    if ($n % 2 == 0 || $n == 1) {
         return 0;
     }
-    for (my $i=3; $i < int(sqrt($n)) + 1; $i++)
-    {
-        if ($n % $i == 0)
-        {
+    for (my $i=3; $i < int(sqrt($n)) + 1; $i++) {
+        if ($n % $i == 0) {
             return 0;
         }
     }
@@ -28,19 +23,18 @@ sub is_prime
 
 sub all_primes
 {
-    my $limit;
-    ($limit) = @_;
-
+    my ($limit) = @_;
     my $total = 0;
-    for (my $i=2; $i < $limit; $i++)
-    {
-        if ((is_prime($i)))
-        {
+    for (my $i=2; $i < $limit; $i++) {
+        if ((is_prime($i))) {
             $total += $i
         }
     }
     return $total;
 }
 
+my $start = clock();
 my $answer = all_primes(2000000);
+my $stop = clock();
 print "Answer: ", $answer, "\n";
+printf "Time: %.5f\n", $stop - $start;
