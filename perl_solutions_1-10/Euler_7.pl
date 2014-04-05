8272b15e59dd33798892e0f1c1be16184cb93dac
Tim Konick <konick781@gmail.com>
2014-04-04 22:19:18 -0400

    fixed up function

--- perl_solutions_1-10/Euler_7.pl
+++ perl_solutions_1-10/Euler_7.pl
@@ -1,24 +1,20 @@
 use strict;
 use warnings;
 use Math::Complex;
+use Time::HiRes qw( clock );
 
 
 sub is_prime
 {
-    my $n;
-    ($n) = @_;
-    if ($n == 2)
-    {
+    my ($n) = @_;
+    if ($n == 2) {
         return 1;
     }
-    if ($n % 2 == 0 || $n == 1)
-    {
+    if ($n % 2 == 0 || $n == 1) {
         return 0;
     }
-    foreach my $i (3..int(sqrt($n)) + 1)
-    {
-        if ($n % $i == 0)
-        {
+    foreach my $i (3..int(sqrt($n)) + 1) {
+        if ($n % $i == 0) {
             return 0;
         }
     }
@@ -27,21 +23,20 @@ sub is_prime
 
 sub find_prime
 {
-    my $prime_num;
-    ($prime_num) = @_;
-
+    my ($prime_num) = @_;
     my $num_prime = 0;
     my $prime = 1;
-    while ($num_prime < $prime_num)
-    {
+    while ($num_prime < $prime_num) {
         $prime++;
-        if (is_prime($prime))
-        {
+        if (is_prime($prime)) {
             $num_prime++;
         }
     }
     return $prime;
 }
 
+my $start = clock();
 my $answer = find_prime(10001);
+my $stop = clock();
 print "Answer: ", $answer, "\n";
+printf "Time: %.5f\n", $stop - $start;
