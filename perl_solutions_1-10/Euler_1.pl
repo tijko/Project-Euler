f729c3e22b1336609d176934990beb0000e99811
Tim Konick <konick781@gmail.com>
2014-04-04 21:02:27 -0400

    use time()

--- perl_solutions_1-10/Euler_1.pl
+++ perl_solutions_1-10/Euler_1.pl
@@ -1,23 +1,22 @@
 use strict;
 use warnings;
+use Time::HiRes qw( clock );
 
-sub three_five_factors {
 
-    my ($low, $high);
-    ($low, $high) = @_;
-
+sub three_five_factors 
+{
+    my ($low, $high) = @_;
     my $total = 0;
-    my $i;
-
-    for $i ($low..$high)
-    {
-        if ($i % 3 == 0 || $i % 5 == 0)
-        {
+    for (my $i = $low; $i < $high; $i++) {
+        if ($i % 3 == 0 || $i % 5 == 0) {
             $total += $i;
         }
     }
     return $total;
 }
 
-my $answer = three_five_factors(0, 999);
+my $start = clock();
+my $answer = three_five_factors(0, 1000);
+my $stop = clock();
 print "Answer: ", $answer, "\n";
+printf "Time: %.5f\n", $stop - $start;
