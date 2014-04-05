7a72939f4b153cfb58fe1b7d0ee1e353e1ad92af
Tim Konick <konick781@gmail.com>
2014-04-04 21:38:47 -0400

    adjust formatting

--- perl_solutions_1-10/Euler_3.pl
+++ perl_solutions_1-10/Euler_3.pl
@@ -1,23 +1,20 @@
 use strict;
 use warnings;
 use Math::Complex;
+use Time::HiRes qw( clock );
 
-sub primes
+
+sub is_prime
 {
-    my ($n);
-    ($n) = @_;
-    if ($n == 2)
-    {
+    my ($n) = @_;
+    if ($n == 2) {
         return 1;
     }
-    if ($n % 2 == 0 || $n == 1)
-    {
+    if ($n % 2 == 0 || $n == 1) {
         return 0;
     }
-    foreach my $i (3..int(sqrt($n)) + 1)
-    {
-        if ($n % $i == 0)
-        {
+    for (my $i = 3; $i < int(sqrt($n)) + 1; $i++) {
+        if ($n % $i == 0) {
             return 0;
         }
     }
@@ -26,18 +23,18 @@ sub primes
 
 sub largest_factor
 {
-    my $factor;
-    ($factor) = @_;
+    my ($factor) = @_;
     my $high = 0;
-    foreach my $i (1..int(sqrt($factor)))
-    {
-        if ($factor % $i == 0 && (primes($i)))
-        {
+    foreach my $i (1..int(sqrt($factor))) {
+        if ($factor % $i == 0 && (is_prime($i))) {
             $high = $i;
         }
     }
     return $high;
 }
 
+my $start = clock();
 my $answer = largest_factor(600851475143);
+my $stop = clock();
 print "Answer: ", $answer, "\n";
+printf "Time: %.5f\n", $stop - $start;
