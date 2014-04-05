41517d4fd760f33521616a05ec39208a404eb87b
Tim Konick <konick781@gmail.com>
2014-04-04 21:29:35 -0400

    tightiing up loop

--- perl_solutions_1-10/Euler_4.pl
+++ perl_solutions_1-10/Euler_4.pl
@@ -1,17 +1,16 @@
 use strict;
 use warnings;
+use Time::HiRes qw( clock );
 
 
 sub palindrome
 {
     my $high = 0;
     my $product;
-    foreach my $i (100..999)
-    {
+    foreach my $i (100..999) {
         foreach my $j (100..999) {
             $product = $i * $j;
-            if ($product eq reverse($product) && $product > $high)
-            {
+            if ($product eq reverse($product) && $product > $high) {
                 $high = $product;
             }
         }
@@ -19,5 +18,8 @@ sub palindrome
     return $high;
 }
 
+my $start = clock();
 my $answer = palindrome();
+my $stop = clock();
 print "Answer: ", $answer, "\n";
+printf "Time: %.5f\n", $stop - $start;
