a9e816d1f8bf241133145a71e79e120390d6bf65
Tim Konick <konick781@gmail.com>
2014-04-04 21:37:59 -0400

    fix formatting

--- perl_solutions_1-10/Euler_5.pl
+++ perl_solutions_1-10/Euler_5.pl
@@ -1,28 +1,27 @@
 use strict;
 use warnings;
+use Time::HiRes qw( clock );
+
 
 sub range_divisor
 {
-    my ($low, $high);
-    ($low, $high) = @_;
-
+    my ($low, $high) = @_;
     my $start = $high;
-    while (1)
-    {
-        foreach my $i ($low..$high)
-        {
-            if ($start % $i != 0)
-            {
+    while (1) {
+        foreach my $i ($low..$high) {
+            if ($start % $i != 0) {
                 $start += $high;
                 last;
             }
-            elsif ($i == $high)
-            {
+            elsif ($i == $high) {
                 return $start;
             }
         }
     }
 }
 
+my $start = clock();
 my $answer = range_divisor(1, 20);
+my $stop = clock();
 print "Answer: ", $answer, "\n";
+printf "Time: %.5f\n", $stop - $start;
