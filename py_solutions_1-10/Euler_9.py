0c9081d56bbacb27db05ee60e7cb980bbbfe1a3f
Tim Konick <konick781@gmail.com>
2014-04-04 22:10:17 -0400

    fix return value

--- py_solutions_1-10/Euler_9.py
+++ py_solutions_1-10/Euler_9.py
@@ -11,7 +11,7 @@ def euler_9():
         b = a + 1
         for b in xrange(b, 3000):
             if sum([a, b, (math.sqrt(a**2 + b**2))]) == 1000:
-                return ("Answer: %s" % int((a * b * (math.sqrt(a**2 + b**2)))))
+                return int((a * b * (math.sqrt(a**2 + b**2))))
             elif sum([a, b, (math.sqrt(a**2 + b**2))]) > 1000:
                 break
 
