447f0d4073979c4bae4889d8144d0a6a77e47a3e
Tim Konick <konick781@gmail.com>
2014-04-14 21:31:07 -0400

    perl solution #18

--- perl_solutions_11-20/Euler_18.pl
+++ perl_solutions_11-20/Euler_18.pl
@@ -3,8 +3,35 @@ use warnings;
 use Time::HiRes qw( clock );
 
 
+sub tri_path
+{
+    my @triangle;
+    my $path = "/home/tijko/Project_Euler/euler_txt/triangle1.txt";
+    open (my $tri, $path);
+    while (my $i = <$tri>) {
+        chomp($i);
+        my @row = split(" ", $i);
+        @triangle = (@triangle, \@row);
+    }
+    close $tri;
+    my $total = $triangle[0][0] + 10;
+    my $col = 0;
+    my $row = 1;
+    while ($row < scalar(@triangle)) {
+        if ($triangle[$row][$col] < $triangle[$row][$col + 1]) {
+            $total += $triangle[$row][$col + 1];
+            $col += 1;
+            $row += 1;
+            next;
+        }
+        $total += $triangle[$row][$col];
+        $row += 1;
+    }
+    return $total;
+}
+
 my $start = clock();
-my $answer = ;
+my $answer = tri_path();
 my $stop = clock();
 print "Answer: $answer\n";
-printf "Time: %.5f\n", $stop -$start;
+printf "Time: %.5f\n", $stop - $start;
