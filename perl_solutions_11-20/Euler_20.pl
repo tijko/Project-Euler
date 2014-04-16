1ca9334d4015b533757c48ffa3cc73c892afc360
Tim Konick <konick781@gmail.com>
2014-04-15 21:10:25 -0400

    perl solution #20

--- perl_solutions_11-20/Euler_20.pl
+++ perl_solutions_11-20/Euler_20.pl
@@ -1,10 +1,23 @@
 use strict;
 use warnings;
 use Time::HiRes qw( clock );
+use Math::BigInt;
 
+
+sub hundredfac_sum
+{
+    my $total = 0;
+    my $hundredstr = "100";
+    my $hundred = Math::BigInt->new($hundredstr);
+    $hundred->bfac();
+    foreach my $i (split(//, "$hundred")) {
+        $total += $i;
+    }
+    return $total;
+}
 
 my $start = clock();
-my $answer = ;
+my $answer = hundredfac_sum();
 my $stop = clock();
 print "Answer: $answer\n";
 printf "Time: %.5f\n", $stop -$start;
