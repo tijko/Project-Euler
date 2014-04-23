e03ccfe4bd44f9b289f5ad0bd2d5155918a29b57
Tim Konick <konick781@gmail.com>
2014-04-22 21:42:28 -0400

    perl solutions #25

---
+++ perl_solutions_21-30/Euler_25.pl
@@ -0,0 +1,31 @@
+use strict;
+use warnings;
+use Math::BigInt;
+use Time::HiRes qw( clock );
+
+
+sub milli_fib
+{
+    my $x = Math::BigInt->new("1");
+    my $y = Math::BigInt->new("0");
+    my $x0 = $x->copy();
+    my $y0 = $y->copy();
+    my $x1 = $x->copy();
+    my $z = 0;
+    while (length($x) < 1000) {
+        my $x0 = $x1->bmul(2);
+        $x0->badd($y);
+        my $y0 = $y->badd($x);
+        $x = $x0->copy();
+        $y = $y0->copy();
+        $x1 = $x->copy();
+        $z +=2; 
+    }
+    return $z;
+}
+
+my $start = clock();
+my $answer = milli_fib();
+my $stop = clock();
+print "Answer: $answer\n";
+printf "Time: %.5f\n", $stop - $start;
