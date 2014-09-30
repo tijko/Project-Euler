a0d4d071ce52dc056db64c37268e1d0db6ae7784
Tim Konick <konick781@gmail.com>
2014-09-30 14:14:52 -0400

    fixed error in initialization

--- c_solutions_1-10/Euler_2.c
+++ c_solutions_1-10/Euler_2.c
@@ -4,32 +4,31 @@
 
 int fib_sum(long limit) {
 
-    int a1, b1 = 1;
-    int b2, total = 0;
+    int a1, b1;
+    long b2, total;
+
+    a1 = 1; b1 = 1;
+    b2 = 0; total = 0;
 
     while (b2 <= limit) {
         b2 = a1 + b1;
         a1 = b1;
         b1 = b2;
-        if (b2 % 2 == 0) {
-            total += b2;
-        }
+        total += b2 % 2 == 0 ? b2 : 0;
     }
+
     return total;
 }
 
 int main(void) {
 
     clock_t start, stop;
-
     start = clock();
 
     const long limit = 4000000;
-
     long ans = fib_sum(limit);
 
     printf ("Answer: %ld\n", ans);
-
     stop = clock();
     printf ("Time: %f\n", ((float)stop - (float)start) / CLOCKS_PER_SEC);
 
