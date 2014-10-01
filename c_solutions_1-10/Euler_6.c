8490272410884ece53d4c661e4ed90556bb784c1
Tim Konick <konick781@gmail.com>
2014-09-30 21:07:47 -0400

    expr in `for` statement

--- c_solutions_1-10/Euler_6.c
+++ c_solutions_1-10/Euler_6.c
@@ -3,26 +3,23 @@
 #include <time.h>
 
 
-int main(void) {
-
+int main(void) 
+{
     clock_t start, stop;
-
     start = clock();
+
     unsigned long total = 0;
 
-    int i;
-    for (i=0; i <= 100; i++) {
-        total += i;
-    }
-    total = pow (total, 2);
-
-    int j;
-    for (j=0; j <= 100; j++) {
-        total -= pow (j, 2);
-    }
+    int i, j;
+    for (i=0; i <= 99; i++, total += i) 
+        ;
+    total = pow(total, 2);
+    for (j=0; j <= 99; j++, total -= pow(j, 2)) 
+        ;
 
     stop = clock();
     printf ("Answer: %ld\n", total);
     printf ("Time: %f\n", ((float)stop - (float)start) / CLOCKS_PER_SEC);
+
     return 0;
 }
