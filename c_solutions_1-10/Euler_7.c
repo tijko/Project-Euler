272a7b14c1e3b6b1825415366940d8085a4af1a6
Tim Konick <konick781@gmail.com>
2014-10-02 11:26:00 -0400

    use for-loop in place of while-loop

--- c_solutions_1-10/Euler_7.c
+++ c_solutions_1-10/Euler_7.c
@@ -3,39 +3,34 @@
 #include <math.h>
 
 
-int prime(unsigned long num) {
+int is_prime(unsigned long num) {
 
     unsigned long j;
-    unsigned long new = sqrt(num) + 1;
+    unsigned long nroot = sqrt(num) + 1;
 
-    for (j=3; j < new; j++) {
-        if (num % j == 0) {
+    for (j=3; j < nroot; j++) {
+        if (num % j == 0) 
             return 0;
-        }
     }
-    return num;
+
+    return 1;
 }
 
 int main(void) {
 
     clock_t start, stop;
-
     start = clock();
-    int count = 4;
-    unsigned long can, cur_num;
-    cur_num = 11;    
-    while (count < 10001) {
-        if (cur_num % 2 != 0) {
-            can = prime(cur_num);
-            if (can > 0) {
-                count++;
-            }
-        }
-        cur_num++;
-    }  
 
-    printf ("Answer: %ld\n", can);
+    unsigned long curr, cnt;
+
+    for (curr=3, cnt=1; cnt < 10001; curr+=2) {
+        if (is_prime(curr))
+            cnt++;
+    }
+
+    printf ("Answer: %ld\n", curr-2);
     stop = clock();
     printf ("Time: %f\n", ((float)stop - (float)start) / CLOCKS_PER_SEC);
+
     return 0;
 }
