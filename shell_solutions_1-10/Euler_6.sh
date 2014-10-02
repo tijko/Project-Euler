9e1c69159301e6fa761ccb062fc2248b0562acc6
Tim Konick <konick781@gmail.com>
2014-10-02 11:12:12 -0400

    euler problem 6 shell solution

--- shell_solutions_1-10/Euler_6.sh
+++ shell_solutions_1-10/Euler_6.sh
@@ -4,4 +4,15 @@
 # natural numbers and the square of the sum?
 
 
-function squared_hundred
+function squared_hundred()
+{
+    sum_all_squares=0
+    square_sum=0
+    for i in {1..100}; do
+        (( square_sum+=$i ))
+        (( sum_all_squares+=$i**2 ))
+    done
+    echo $(( $((square_sum**2)) - $sum_all_squares ))
+}
+
+squared_hundred
