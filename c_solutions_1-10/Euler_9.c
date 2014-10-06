26458b58f5d99ea113fd83862b8a2b9f53f5b9e3
Tim Konick <konick781@gmail.com>
2014-10-06 10:25:02 -0400

    use `goto` statement

--- c_solutions_1-10/Euler_9.c
+++ c_solutions_1-10/Euler_9.c
@@ -8,24 +8,24 @@ int main(void) {
     clock_t start, stop;
     start = clock();
 
-    double a, b;
-    long c;
+    float a, b, c;
     
     for (a=1, b=2; a <= 3000; a++, b=(a+1)) {
         for (; b < 3000; b++) {
-            if (a + b + (sqrt (pow (a, 2) + pow (b, 2))) == 1000) {
-                c = a * b * (sqrt (pow (a, 2) + pow (b, 2)));
+            c = a + b + sqrt(pow(a, 2) + pow(b, 2));
+            if (c == 1000) {
+                c = a * b * sqrt(pow(a, 2) + pow(b, 2));
                 goto found_triplet;
-            }
-            if (a + b + (sqrt (pow (a, 2) + pow (b, 2))) > 1000) 
+            } else if (c > 1000) {
                 break;
+            }
         }
     }
 
     found_triplet:
 
     stop = clock();
-    printf ("Answer: %ld\n", c);
+    printf ("Answer: %ld\n", (long) c);
     printf ("Time: %f\n", ((float)stop - (float)start) / CLOCKS_PER_SEC);
 
     return 0;
