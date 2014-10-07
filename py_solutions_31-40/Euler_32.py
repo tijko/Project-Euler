e2038c9bef3f099f908655aa59ac8dd5f3501e75
Tim Konick <konick781@gmail.com>
2014-10-07 18:28:59 -0400

    sm edit

--- py_solutions_31-40/Euler_32.py
+++ py_solutions_31-40/Euler_32.py
@@ -10,9 +10,9 @@ start = timeit.default_timer()
 def euler_32():
     pandigitals = set()
     pandigital = {'1', '2', '3', '4', '5', '6', '7', '8', '9'}
-    for x,y in itertools.product(xrange(2, 100), xrange(100, 5000)):
+    for x, y in itertools.product(xrange(2, 100), xrange(100, 5000)):
         digi_str = ''.join(map(str, [x * y, x, y]))
-        if len(digi_str) == 9 and {j for j in digi_str} == pandigital:
+        if len(digi_str) == 9 and {i for i in digi_str} == pandigital:
             pandigitals.add(x * y)
     return sum(pandigitals)
 
