4d030b72d7428334bbbeb6ed4d6f947d5e6326a9
Tim Konick <konick781@gmail.com>
2014-10-08 12:20:33 -0400

    minor edit

--- c_solutions_11-15/Euler_12.c
+++ c_solutions_11-15/Euler_12.c
@@ -6,7 +6,6 @@
 int main(void) {
 
     clock_t start, stop;
-
     start = clock();
    
     int base = 1001;
@@ -16,20 +15,18 @@ int main(void) {
     long double root;
 
     int i;
-    for (i=1; i <= base; i++) {
+    for (i=1; i <= base; i++) 
         tri_num += i;
-    }        
 
     while (divisors <= 500) {
-        root = sqrt (tri_num);
+        root = sqrt(tri_num);
         for (i=2; i <= root + 1; i++) {
-            if (tri_num % i == 0) {
+            if (tri_num % i == 0) 
                 divisors += 2;    
-            }
         }
+
         if (divisors <= 500) {
-            base++;
-            tri_num += base;
+            tri_num += (++base);
             divisors = 2;
         }
     } 
