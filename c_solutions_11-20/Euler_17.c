5d024a13f7ee1a9c883ebb111af1e38b0cf943c0
Tim Konick <konick781@gmail.com>
2014-10-15 22:26:23 -0400

    nested ternaries

--- c_solutions_11-20/Euler_17.c
+++ c_solutions_11-20/Euler_17.c
@@ -17,46 +17,36 @@ int main(int argc, char *argv[])
     int count_to = 1000;
     int letter_count = 0;
     int tens_offset = 2;
-    char *number_words[4][9] = {{"one", "two", "three", "four", 
+    char *number_words[5][10] = {{"", "one", "two", "three", "four", 
                                   "five", "six", "seven", "eight", "nine"},
+                                 {"ten", "eleven", "twelve", "thirteen", 
+                                  "fourteen", "fifteen", "sixteen", 
+                                  "seventeen", "eighteen", "nineteen"},
                                  {"twenty", "thirty", "forty", "fifty", 
                                   "sixty", "seventy", "eighty", "ninety"},
-                                 {"hundred", "hundredand"}, {"thousand"}};
+                                 {"", "hundred", "hundredand"}, {"", "thousand"}};
  
-    char *tens[10] = {"ten", "eleven", "twelve", "thirteen", "fourteen", 
-                      "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
 
-    int i, tmp;
-    for (i=1; i <= count_to; i++) {
-        if (i / 1000) {
-            letter_count += strlen(number_words[0][(i / 1000) - 1]);
-            letter_count += strlen(number_words[3][0]);
-        } else if (i / 100) {
-            letter_count += strlen(number_words[0][(i / 100) - 1]);
-            if (i % 10) {
-                letter_count += strlen(number_words[2][1]);
-                tmp = i % 100;
-                if (tmp / 10 == 1) {
-                    letter_count += strlen(tens[(tmp % 10) - 1]);
-                } else {
-                    letter_count += strlen(number_words[1][(tmp / 10) - tens_offset]);
-                }
-                if (tmp % 10)
-                    letter_count += strlen(number_words[0][(tmp % 10) - 1]);
-            } else {
-                letter_count += strlen(number_words[2][0]);
-            }
-        } else if (i / 10) {
-            if (i / 10 == 1) {
-                letter_count += strlen(tens[i % 10]);
-            } else {
-                letter_count += strlen(number_words[1][(i / 10) - tens_offset]);
-                if (i % 10)
-                    letter_count += strlen(number_words[0][(i % 10) - 1]);
-            }
-        } else {
-            letter_count += strlen(number_words[0][(i % 10) - 1]);
-        }
+    int i, current;
+    for (i=1, current=i; i <= count_to;  i++, current=i) {
+
+        letter_count += strlen(number_words[0][current / 1000]);
+        letter_count += strlen(number_words[4][current / 1000]);
+
+        current %= 1000;
+        
+        letter_count += current / 100 > 0 ? current % 100 > 0 ? strlen(number_words[0][current / 100]) + 
+                                                                strlen(number_words[3][2]) : 
+                                                                strlen(number_words[0][current / 100]) +
+                                                                strlen(number_words[3][1]) :
+                                                                strlen(number_words[3][0]);
+        
+        current %= 100;
+
+        letter_count += current / 10 > 0 ? current / 10 == 1 ? strlen(number_words[1][current % 10]) :
+                                           strlen(number_words[2][(current / 10) - tens_offset]) + 
+                                           strlen(number_words[0][current % 10]) :
+                                           strlen(number_words[0][current % 10]);
     }
    
     stop = clock();
