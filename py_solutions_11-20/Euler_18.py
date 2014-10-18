f6f129ce1acf07ce193f00a609094d66cee454b3
Tim Konick <konick781@gmail.com>
2014-10-18 13:13:02 -0400

    fixed error in algorithm

--- py_solutions_11-20/Euler_18.py
+++ py_solutions_11-20/Euler_18.py
@@ -10,15 +10,19 @@ def euler_18():
     path = os.getcwd().strip('py_solutions_11-20')
     with open(path + '/euler_txt/triangle1.txt') as f:
         tri = [map(int, i.split()) for i in f.readlines()]
-    col = 0
-    total = tri[0][0] + 10
-    for row in xrange(1, len(tri)):
-        if tri[row][col] > tri[row][col + 1]:
-            total += tri[row][col]
-            continue
-        total += tri[row][col + 1]
-        col += 1
+    row = col = total = 0
+    while row < len(tri) - 2:
+        total += tri[row][col]
+        col += (1 if tri[row + 1][col + 1] + 
+                max(tri[row + 2][col + 1], 
+                    tri[row + 2][col + 2]) > 
+                    tri[row + 1][col] + 
+                max(tri[row + 2][col], 
+                    tri[row + 2][col + 1]) else 0)
+        row += 1
+    total += (tri[row][col] + max(tri[row + 1][col], tri[row + 1][col + 1]))
     return total
+
 
 print "Answer: %s" % euler_18()
 stop = timeit.default_timer()
