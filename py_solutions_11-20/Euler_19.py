030905521e658452bd390f6aa8d306543ddf6c73
Tim Konick <konick781@gmail.com>
2014-10-20 23:51:57 -0400

    make a generator func.

--- py_solutions_11-20/Euler_19.py
+++ py_solutions_11-20/Euler_19.py
@@ -1,4 +1,5 @@
-# find the number sundays that fell on the first day of the month in between 1901 - 2000 
+# find the number sundays that fell on the first day of the month in between 
+# 1901 - 2000. 
 
 import timeit
 from itertools import cycle
@@ -6,21 +7,18 @@ from itertools import cycle
 
 start = timeit.default_timer()
 
-def euler_19():
-    sundays = 0 
+def euler_19(start, stop):
+    leap_yr = xrange(1, 30)
     weekday = cycle(xrange(7))
-    for i in xrange(1901, 2000):
-        if i % 4 == 0: 
-            Feb = xrange(1, 30) 
-        else: 
-            Feb = xrange(1, 29) 
+    for i in xrange(start, stop):
+        Feb = xrange(1, 29) if i % 4 != 0 else leap_yr
         Jan = Mar = May = Jul = Aug = Oct = Dec = xrange(1, 32)    
         Apr = Jun = Sep = Nov = xrange(1, 31) 
         year = [Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec] 
-        sundays += sum([1 for mos in xrange(12) if 
-                        zip(year[mos], weekday)[0] == (1, 6)]) 
-    return sundays 
+        yield sum([1 for mos in xrange(12) if 
+                   zip(year[mos], weekday)[0] == (1, 6)])
 
-print "Answer: %s" % euler_19()
+
+print "Answer: %s" % sum([sunday for sunday in euler_19(1901, 2000)])
 stop = timeit.default_timer()
 print "Time: %f" % (stop - start)
