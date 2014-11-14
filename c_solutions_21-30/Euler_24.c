bf881d5ef5a9fdf209def45858810e5a342e9f09
Tim Konick <konick781@gmail.com>
2014-11-14 17:56:33 -0500

    Euler c solution #24

---
+++ c_solutions_21-30/Euler_24.c
@@ -0,0 +1,82 @@
+/* 
+ * What is the millionth lexicographic permutation of the digits
+ * 0, 1, 2, 3, 4, 5, 6, 7, 8, and 9?
+ *
+ */
+
+#include <time.h>
+#include <stdio.h>
+
+#define PERMUTATION_SIZE 10 
+#define PERMUTATION_LIMIT 1000000
+
+
+void sort(int permutation[], int idx)
+{
+    int i, j, curr;
+    for (i=idx; i < PERMUTATION_SIZE - 1; i++) {
+        curr = permutation[i + 1];
+        j = i;
+        while (j > idx && curr < permutation[j]) {
+            permutation[j + 1] = permutation[j];
+            permutation[j] = curr;
+            j--;
+        }
+        permutation[j + 1] = curr;
+    }
+}
+
+void swap(int permutation[], int idx1, int idx2)
+{
+    int tmp;
+    tmp = permutation[idx1];
+    permutation[idx1] = permutation[idx2];
+    permutation[idx2] = tmp;    
+}
+
+int next_largest_idx(int permutation[])
+{
+    int i;
+    for (i=PERMUTATION_SIZE - 1; i > 0; i--)
+        if (permutation[i] > permutation[i - 1])
+            return i - 1;
+    return i;
+}
+
+int last_largest_idx(int permutation[], int idx)
+{
+    int i;
+    for (i=PERMUTATION_SIZE - 1; i >= idx; i--)
+        if (permutation[i] > permutation[idx])
+            return i;
+    return i;
+}
+
+int main(int argc, char *argv[])
+{
+    clock_t start, stop;
+    start = clock();
+
+    int one_to_ten[PERMUTATION_SIZE] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int first_idx, second_idx, count;
+
+    int i;
+
+    for (count=0; count < PERMUTATION_LIMIT - 1; count++) {
+        first_idx = next_largest_idx(one_to_ten);
+        second_idx = last_largest_idx(one_to_ten, first_idx);
+        swap(one_to_ten, first_idx, second_idx);
+        sort(one_to_ten, first_idx);
+    }
+
+    stop = clock();
+    
+    printf("Answer: ");
+
+    for (i=0; i < PERMUTATION_SIZE; i++)
+        printf("%d", one_to_ten[i]);
+
+    printf("\nTime: %f\n", ((float) stop - (float) start) / CLOCKS_PER_SEC);
+
+    return 0;
+}
