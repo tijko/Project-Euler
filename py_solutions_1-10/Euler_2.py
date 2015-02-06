6cffd9f7cd97e3cb1b75e7968fbe7e1a6de4f9a3
Tim Konick <konick781@gmail.com>
2015-02-06 00:26:04 -0500

    py3 compatible

--- py_solutions_1-10/Euler_2.py
+++ py_solutions_1-10/Euler_2.py
@@ -1,28 +1,24 @@
 # sum of even fibonacci numbers below 4 million? 
 
+from __future__ import print_function
 import timeit
 
 
 start = timeit.default_timer()
-
-#def euler_2():
-#    a1 = 1
-#    b1 = 1
-#    b2 = 0
-#    limit = 4000000
-#    while b2 <= limit:
-#        b2 = a1 + b1
-#        a1 = b1
-#        b1 = b2
-#        if b2 % 2 == 0:
-#            yield b2
-
+def euler_2():
+    a, b = 0, 1
+    while b <= 4000000:
+        a, b = b, a + b
+        if b % 2 == 0:
+            yield b
+"""
 def euler_2(t=0, n=2, ln=1):
     if n >= 4000000: return t
     elif n % 2 == 0: return euler_2(t + n, n + ln, n)
     return euler_2(t, n + ln, n)
+"""
 
-print "Answer: %s" % euler_2() 
-#print "Answer: %s" % sum([i for i in euler_2()])
+#print("Answer: {}".format(euler_2()))
+print("Answer: {}".format(sum([i for i in euler_2()])))
 stop = timeit.default_timer()
-print "Time: %f" % (stop - start)
+print("Time: {0:9.5f}".format(stop - start))
