d9d0e78f2e6889d417b3cf80aacc1976350136fd
Tim Konick <konick781@gmail.com>
2015-02-14 00:18:45 -0500

    Py3 compatible

--- py_solutions_1-10/Euler_6.py
+++ py_solutions_1-10/Euler_6.py
@@ -1,13 +1,19 @@
 # Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum? 
 
+from __future__ import print_function
+
 import timeit
 
+try:
+    range = xrange
+except NameError:
+    pass
 
 start = timeit.default_timer()
 
 def euler_6():
-    return sum(xrange(1, 101))**2 - sum(map(lambda x: x**2, xrange(1, 101)))
+    return sum(range(1, 101))**2 - sum(map(lambda x: x**2, range(1, 101)))
 
-print "Answer: %s" % euler_6()
+print("Answer: {}".format(euler_6()))
 stop = timeit.default_timer()
-print "Time: %f" % (stop - start)
+print("Time: {0:9.5f}".format(stop - start))
