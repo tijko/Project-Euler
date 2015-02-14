18b27517e9ef8490f4b923a596269e1ce9672885
Tim Konick <konick781@gmail.com>
2015-02-14 00:25:42 -0500

    Py3 compatible

--- py_solutions_1-10/Euler_9.py
+++ py_solutions_1-10/Euler_9.py
@@ -1,21 +1,27 @@
 # Find the only pythagorean triplet that a + b + c = 1000 
 
+from __future__ import print_function
+
 import math
 import timeit
 
+try:
+    range = xrange
+except NameError:
+    pass
 
 start = timeit.default_timer()
 
 def euler_9():
-    for a in xrange(1, 3000):
+    for a in range(1, 3000):
         b = a + 1
-        for b in xrange(b, 3000):
+        for b in range(b, 3000):
             c = sum([a, b, (math.sqrt(a**2 + b**2))])
             if c == 1000:
                 return int(a * b * (math.sqrt(a**2 + b**2)))
             elif c > 1000:
                 break
 
-print 'Answer: %s' % euler_9()
+print('Answer: {}'.format(euler_9()))
 stop = timeit.default_timer()
-print 'Time: %f' % (stop - start)
+print('Time: {0:9.5f}'.format(stop - start))
