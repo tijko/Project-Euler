6cb8bc0bd46788f39286d94ffbd33fb11f7f4370
Tim Konick <konick781@gmail.com>
2015-02-14 00:13:37 -0500

    Py3 compatible

--- py_solutions_1-10/Euler_4.py
+++ py_solutions_1-10/Euler_4.py
@@ -1,18 +1,24 @@
 # Find the largest palindrome number made from the product of two 3-digit numbers?
 
+from __future__ import print_function
+
 import itertools
 import operator 
 import timeit
 
+try:
+    range = xrange
+except NameError:
+    pass
 
 start = timeit.default_timer()
 
 def euler_4():
-    canidates = itertools.combinations(xrange(100, 1000), 2)
+    canidates = itertools.combinations(range(100, 1000), 2)
     palindrome_check = lambda x: (str(operator.mul(*x)) ==  
                                   str(operator.mul(*x))[::-1])
     return max([operator.mul(*i) for i in canidates if palindrome_check(i)])
 
-print "Answer: %s" % euler_4()
+print("Answer: {}".format(euler_4()))
 stop = timeit.default_timer()
-print "Time: %f" % (stop - start)
+print("Time: {0:9.5f}".format(stop - start))
