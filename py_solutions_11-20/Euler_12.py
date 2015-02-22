eead6060b7bc664dc17aad20cf99d952e7254a78
Tim Konick <konick781@gmail.com>
2015-02-21 23:29:51 -0500

    Py3 compatible

--- py_solutions_11-20/Euler_12.py
+++ py_solutions_11-20/Euler_12.py
@@ -1,15 +1,22 @@
 # What is the value of the first triangle number to have over five-hundred divisors?
 
+from __future__ import print_function
+
 import math
 import timeit
 import sys
 
 
+try:
+    range = xrange
+except NameError:
+    pass
+
 sys.setrecursionlimit(15000)
 start = timeit.default_timer()
 
 def factor_gen(tri):
-    for i in xrange(2, int(math.sqrt(tri)) + 1):
+    for i in range(2, int(math.sqrt(tri)) + 1):
         if tri % i == 0:
             yield i
 
@@ -18,6 +25,6 @@ def euler_12(n, tri):
         return tri
     return euler_12(n + 1, sum([tri, n, 1]))
 
-print "Answer: %s" % euler_12(1001, sum(xrange(1002)))
+print("Answer: {}".format(euler_12(1001, sum(range(1002)))))
 stop = timeit.default_timer()
-print "Time: %f" % (stop - start)
+print("Time: {0:9.5f}".format(stop - start))
