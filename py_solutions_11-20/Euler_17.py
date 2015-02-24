a17191043b615be4872b60329a1bed314b2081db
Tim Konick <konick781@gmail.com>
2015-02-23 22:32:24 -0500

    py3 compatible

--- py_solutions_11-20/Euler_17.py
+++ py_solutions_11-20/Euler_17.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
+
 '''
 If the numbers 1 to 5 are written out in words: one, two, three, four, five, 
 then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
@@ -31,28 +33,28 @@ def euler_17():
     while number < 1000:
         number += 1
         curr = number
-        if curr / 1000:
-            letter_count += (len(number_words[0][curr / 1000]) +
+        if curr // 1000:
+            letter_count += (len(number_words[0][curr // 1000]) +
                              len(number_words[4][0]))
         curr %= 1000
-        if curr / 100 and not curr % 100:
-            letter_count += (len(number_words[0][curr / 100]) + 
+        if curr // 100 and not curr % 100:
+            letter_count += (len(number_words[0][curr // 100]) + 
                              len(number_words[3][0]))
             continue
-        if curr / 100:
-            letter_count += (len(number_words[0][curr / 100]) +
+        if curr // 100:
+            letter_count += (len(number_words[0][curr // 100]) +
                              len(number_words[3][1]))
         curr %= 100
-        if curr / 10 == 1:
+        if curr // 10 == 1:
             letter_count += len(number_words[1][curr % 10])
             continue
-        if curr / 10:
-            letter_count += len(number_words[2][(curr / 10) - 2])
+        if curr // 10:
+            letter_count += len(number_words[2][(curr // 10) - 2])
         curr %= 10
         letter_count += len(number_words[0][curr % 10])
     return letter_count
 
 
-print "Answer: %s" % euler_17()
+print("Answer: {}".format(euler_17()))
 stop = timeit.default_timer()
-print "Time: %f" % (stop - start)
+print("Time: {0:9.5f}".format(stop - start))
