54c57b2dc66ae9c8b1b2e55311c4cdb36cb5240d
Tim Konick <konick781@gmail.com>
2015-02-23 22:28:21 -0500

    py3 compatible

--- py_solutions_11-20/Euler_15.py
+++ py_solutions_11-20/Euler_15.py
@@ -1,5 +1,8 @@
 # find all paths in 20 X 20 grid
 
+from __future__ import print_function
+
+
 from math import factorial
 import timeit
 
@@ -9,6 +12,6 @@ start = timeit.default_timer()
 def euler_15():
     return (factorial(40) / (factorial(20) * factorial(20)))
 
-print "Answer: %s" % euler_15()
+print("Answer: {}".format(euler_15()))
 stop = timeit.default_timer()
-print "Time: %f" % (stop - start)
+print("Time: {0:9.5f}".format(stop - start))
