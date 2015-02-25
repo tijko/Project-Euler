51cfcaaa79b0b32c06c59cd0c85da46aeb4e8cb8
Tim Konick <konick781@gmail.com>
2015-02-24 23:33:25 -0500

    py3 compatible

--- py_solutions_21-30/Euler_24.py
+++ py_solutions_21-30/Euler_24.py
@@ -11,18 +11,28 @@ The lexicographic permutations of 0, 1 and 2 are:
 What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 '''
 
+from __future__ import print_function
+
 from itertools import permutations
 import timeit
 
+try:
+    range = xrange
+except NameError:
+    pass
 
 start = timeit.default_timer()
 
 def euler_24():
     pandigital = permutations('0123456789')
-    for _ in xrange(999999):
-        pandigital.next()
-    return ''.join(pandigital.next())
+    if hasattr(pandigital, '__next__'):
+        nxt = pandigital.__next__
+    else:
+        nxt = pandigital.next
+    for _ in range(999999):
+        nxt()
+    return ''.join(nxt())
 
-print "Answer: %s" % euler_24()
+print("Answer: {}".format(euler_24()))
 stop = timeit.default_timer()
-print "Time: %f" % (stop - start) 
+print("Time: {0:9.5f}".format(stop - start))
