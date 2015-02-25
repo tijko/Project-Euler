456e53e7de0159e18d3243bf6f2d854da841aef4
Tim Konick <konick781@gmail.com>
2015-02-24 23:10:41 -0500

    py3 compatible

--- py_solutions_21-30/Euler_21.py
+++ py_solutions_21-30/Euler_21.py
@@ -1,7 +1,14 @@
 # Calculate the sum of all amicable pairs below 10000 
 
+from __future__ import print_function
+
 import timeit
 import functools
+
+try:
+    range = xrange
+except NameError:
+    pass
 
 start = timeit.default_timer()
 
@@ -10,14 +17,14 @@ def mod_zero(dividend, divisor):
 
 def euler_21():
     total = 0
-    for i in xrange(1, 10000):
-        p1 = sum(filter(functools.partial(mod_zero, i), xrange(1, i / 2 + 1)))  
-        p2 = sum(filter(functools.partial(mod_zero, p1), xrange(1, p1 / 2 + 1)))
+    for i in range(1, 10000):
+        p1 = sum(filter(functools.partial(mod_zero, i), range(1, i // 2 + 1)))  
+        p2 = sum(filter(functools.partial(mod_zero, p1), range(1, p1 // 2 + 1)))
         if p2 == i != p1: 
             total += i
     return total
 
 
-print "Answer: %s" % euler_21()     
+print("Answer: {}".format(euler_21()))
 stop = timeit.default_timer()
-print "Time: %f" % (stop - start)
+print("Time: {0:9.5f}".format(stop - start))
