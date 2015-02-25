60ba7284ce4bbeefea5e367264844d7f75f0777b
Tim Konick <konick781@gmail.com>
2015-02-24 23:21:56 -0500

    py3 compatible

--- py_solutions_21-30/Euler_23.py
+++ py_solutions_21-30/Euler_23.py
@@ -1,24 +1,36 @@
 # Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers. 
 
-import timeit
-import itertools
+from __future__ import print_function
 
+import timeit
+
+try:
+    from itertools import ifilterfalse as filterfalse
+except ImportError:
+    from itertools import filterfalse
+
+from itertools import combinations_with_replacement
+
+try:
+    range = xrange
+except NameError:
+    pass
 
 start = timeit.default_timer()
 
 
 def euler_23():
-    abundant = (i for i in xrange(28124) if i < 
-                sum(itertools.ifilterfalse(lambda x, i=i: i % x, xrange(1, i / 2 + 1))))
+    abundant = (i for i in range(28124) if i < 
+                sum(filterfalse(lambda x, i=i: i % x, range(1, i // 2 + 1))))
     abundant_sums = {i for i in combo_gen(abundant)}
-    return sum(abundant_sums.symmetric_difference(xrange(28124)))
+    return sum(abundant_sums.symmetric_difference(range(28124)))
 
 def combo_gen(x):
-     for i in itertools.combinations_with_replacement(x, 2):
+     for i in combinations_with_replacement(x, 2):
          if sum(i) < 28124:
             yield sum(i)
 
 
-print "Answer: %s" % euler_23()
+print("Answer: {}".format(euler_23()))
 stop = timeit.default_timer()
-print "Time: %f" % (stop - start)
+print("Time: {0:9.5f}".format(stop - start))
