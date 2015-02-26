0842bb997ca4b6b4740d5d7b636d974cd76c8e0b
Tim Konick <konick781@gmail.com>
2015-02-26 01:00:05 -0500

    py3 compatible

--- py_solutions_21-30/Euler_30.py
+++ py_solutions_21-30/Euler_30.py
@@ -1,15 +1,21 @@
 # Sum of numbers that can be written as the sum of their fifth power. 
 
+from __future__ import print_function
+
 import timeit
 
+try:
+    range = xrange
+except NameError:
+    pass
 
 start = timeit.default_timer()
 
 def euler_30():
     fifth_pow = lambda x: x**5
-    return sum([i for i in xrange(2, 1000000) if
+    return sum([i for i in range(2, 1000000) if
                 sum(map(fifth_pow, map(int, str(i)))) == i])
 
-print "Answer: %s" % euler_30()
+print("Answer: {}".format(euler_30()))
 stop = timeit.default_timer()
-print "Time: %f" % (stop - start)
+print("Time: {0:9.5f}".format(stop - start))
