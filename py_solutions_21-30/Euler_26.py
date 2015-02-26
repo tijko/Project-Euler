5220f1e7839cdf42032d7c80222dadaa24004e17
Tim Konick <konick781@gmail.com>
2015-02-26 00:50:05 -0500

    py3 compatible

--- py_solutions_21-30/Euler_26.py
+++ py_solutions_21-30/Euler_26.py
@@ -1,8 +1,14 @@
 # Largest sequence of repeating numbers of n < 1000 of 1/d
 
+from __future__ import print_function
+
 import timeit
 import math
 
+try:
+    range = xrange
+except NameError:
+    pass
 
 start = timeit.default_timer()
 
@@ -11,17 +17,17 @@ def is_prime(x):
         return True
     if x % 2 == 0 or x == 1:
         return False
-    for i in xrange(3, int(math.sqrt(x) + 1), 2):
+    for i in range(3, int(math.sqrt(x) + 1), 2):
         if x % i == 0:
             return False
     return True
 
 
 def euler_26():
-    primes_to_onethousand = filter(is_prime, xrange(1, 1000))
+    primes_to_onethousand = list(filter(is_prime, range(1, 1000)))
     return primes_to_onethousand[len(primes_to_onethousand) - 3]
 
 
-print "Answer: %s" % euler_26()
+print("Answer: {}".format(euler_26()))
 stop = timeit.default_timer()
-print "Time: %f" % (stop - start)
+print("Time: {0:9.5f}".format(stop - start))
