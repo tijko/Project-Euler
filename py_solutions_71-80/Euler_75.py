50fb75a5025a762634e56841997a7b4e94379053
Tim Konick <konick781@gmail.com>
2015-12-02 18:03:05 -0500

    Refactor problem #75

--- py_solutions_71-80/Euler_75.py
+++ py_solutions_71-80/Euler_75.py
@@ -1,50 +1,44 @@
-# --*-- coding: utf-8 --*--
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
 
-# Given that L is the length of the wire, for how many values of L ≤ 1,500,000 can exactly one integer sided right angle triangle be formed?
 
-import collections
+# Given that L is the length of the wire, for how many values of 
+# L ≤ 1,500,000 can exactly one integer sided right angle triangle be formed?
+
 import timeit
+import collections
 
+from functools import partial
+from itertools import starmap
+from operator import imul, add
 
 start = timeit.default_timer()
 
 def euler_75():
-    seen = collections.defaultdict(int)
+    triplet_count = collections.defaultdict(int)
+    triplets = [(3, 4, 5)]
     while triplets:
-        triple = triplets.pop(0)
-        a1, b1, c1 = triple
-        triangle1(a1, b1, c1)
-        triangle2(a1, b1, c1)
-        triangle3(a1, b1, c1)
-        (a2, b2, c2) = (a1, b1, c1)
-        while sum([a1, b1, c1]) <= 1500000:
-            seen[sum([a1, b1, c1])] += 1
-            (a1, b1, c1) = map(sum, zip([a1, b1, c1], [a2, b2, c2]))
-    return len([i for i in seen if seen[i] == 1])
+        triplet = triplets.pop(0)
+        triangle(triplets, triplet)
+        a, b, c = triplet
+        while sum([a, b, c]) <= 1500000:
+            triplet_count[sum([a, b, c])] += 1
+            a, b, c = map(sum, zip([a, b, c], triplet))
+    return len([i for i in triplet_count if triplet_count[i] == 1])
 
-def triangle1(a, b, c):
-    a2 = (a - (2 * b)) + (2 * c)
-    b2 = (2 * a) - b + (2 * c)
-    c2 = (2 * a) - (2 * b) + (3 * c)
-    if sum([a2, b2, c2]) <= 1500000:
-        triplets.append([a2, b2, c2])
+def triangle(triplets, triplet):
+    triangles = [[(1, -2, 2), (2, -1, 2), (2, -2, 3)],
+                 [(1, 2, 2), (2, 1, 2), (2, 2, 3)],
+                 [(-1, 2, 2), (-2, 1, 2), (-2, 2, 3)]]
+    zip_trip = partial(zip, triplet)
+    for triangle in triangles:
+        new_triplet = [reduce(add, starmap(imul, i)) 
+                       for i in map(zip_trip, triangle)]
+        if sum(new_triplet) <= 1500000:
+            triplets.append(new_triplet)
 
-def triangle2(a, b, c):
-    a2 = a + (2 * b) + (2 * c)
-    b2 = (2 * a) + b + (2 * c)
-    c2 = (2 * a) + (2 * b) + (3 * c)
-    if sum([a2, b2, c2]) <= 1500000:
-        triplets.append([a2, b2, c2])
 
-def triangle3(a, b, c):
-    a2 = -a + (2 * b) + (2 * c)
-    b2 = (-2 * a) + b + (2 * c)
-    c2 = (-2 * a) + (2 * b) + (3 * c)
-    if sum([a2, b2, c2]) <= 1500000:
-        triplets.append([a2, b2, c2])
-
-    
-triplets = [[3, 4, 5]]
-print "Answer: %s" % euler_75()
-stop = timeit.default_timer()
-print "Time: %f" % (stop - start)
+if __name__ == '__main__': 
+    print "Answer: %s" % euler_75()
+    stop = timeit.default_timer()
+    print "Time: %f" % (stop - start)
