cfe6ab21afcaaf8483d8829b11320c7e83fec721
Tim Konick <konick781@gmail.com>
2016-03-20 14:30:32 -0400

    Rewrite py solution #84

--- py_solutions_81-90/Euler_84.py
+++ py_solutions_81-90/Euler_84.py
@@ -33,20 +33,62 @@ from random import choice, shuffle
 from collections import defaultdict
 
 
+squares = ['go', 'a1', 'cc1', 'a2', 't1', 'r1', 'b1', 'ch1', 'b2', 'b3',
+           'jail', 'c1', 'u1', 'c2', 'c3', 'r2', 'd1', 'cc2', 'd2', 'd3',
+           'fp', 'e1', 'ch2', 'e2', 'e3', 'r3', 'f1', 'f2', 'u2', 'f3',
+           'g2j', 'g1', 'g2', 'cc3', 'g3', 'r4', 'ch3', 'h1', 't2', 'h2']
+visits = defaultdict(int)
+cc_cards = ['go', 'jail'] + [None] * 14 
+ch_cards = (['go', 'jail', 'c1', 'e3', 'h2', 'r1', 'nextr', 
+             'nextr', 'nextu', 'back'] + [None] * 6)
+shuffle(cc_cards); shuffle(ch_cards)
+cc, ch= cycle(cc_cards), cycle(ch_cards)
+
+def community_chest(sq, pos):
+    card = next(cc)
+    if card is None:
+        visits[sq] += 1
+    else:
+        pos = squares.index(card)
+        visits[card] += 1
+    return pos
+
+def chance(sq, pos):
+    card = next(ch)
+    if card is None:
+        visits[sq] += 1
+    elif card.startswith('next'):
+        if card == 'nextr':
+            if pos > 35 or pos < 5:
+                pos = 5
+            elif pos > 5 and pos < 15:
+                pos = 15
+            elif pos > 15 and pos < 25:
+                pos = 25
+            else:
+                pos = 35
+        else:
+            if pos > 28 or pos < 12:
+                pos = 12
+            else:
+                pos = 28
+        visits[squares[pos]] += 1
+    elif card == 'back':
+        pos -= 3
+        sq = squares[pos]
+        if sq == 'cc3':
+            pos = community_chest(sq, pos)
+        else:
+            visits[sq] += 1
+    else:
+        pos = squares.index(card)
+        visits[card] += 1
+    return pos
+
 def euler_84():
-    squares = ['go', 'a1', 'cc1', 'a2', 't1', 'r1', 'b1', 'ch1', 'b2', 'b3',
-               'jail', 'c1', 'u1', 'c2', 'c3', 'r2', 'd1', 'cc2', 'd2', 'd3',
-               'fp', 'e1', 'ch2', 'e2', 'e3', 'r3', 'f1', 'f2', 'u2', 'f3',
-               'g2j', 'g1', 'g2', 'cc3', 'g3', 'r4', 'ch3', 'h1', 't2', 'h2']
-    visits = defaultdict(int)
-    cc_cards = ['go', 'jail'] + [None] * 14 
-    ch_cards = (['go', 'jail', 'c1', 'e3', 'h2', 'r1', 'nextr', 
-                 'nextr', 'nextu', 'back'] + [None] * 6)
-    shuffle(cc_cards); shuffle(ch_cards)
-    cc, ch= cycle(cc_cards), cycle(ch_cards)
     rolls = [(i, j) for i in range(1, 5) for j in range(1, 5)]
     doubles = pos = 0 
-    for _ in range(100000):
+    for _ in range(1000000):
         roll = choice(rolls)
         if roll[0] == roll[1]:
             doubles += 1
@@ -54,58 +96,21 @@ def euler_84():
                 pos = squares.index('jail')
                 visits['jail'] += 1
                 doubles = 0
+                continue
         else:
             doubles = 0 
-            pos += sum(roll)
-            pos = pos % 40
-            sq = squares[pos]
-            if sq.startswith('cc'):
-                card = next(cc)
-                if card is None:
-                    visits[sq] += 1
-                else:
-                    pos = squares.index(card)
-                    visits[card] += 1
-            elif sq.startswith('ch'):
-                card = next(ch)
-                if card is None:
-                    visits[sq] += 1
-                else:
-                    if card.startswith('next'):
-                        if card == 'nextr':
-                            if pos > 35 or pos < 5:
-                                pos = 5
-                            elif pos > 5 and pos < 15:
-                                pos = 15
-                            elif pos > 15 and pos < 25:
-                                pos = 25
-                            else:
-                                pos = 35
-                        else:
-                            if pos > 28 or pos < 12:
-                                pos = 12
-                            else:
-                                pos = 28
-                    elif card == 'back':
-                        pos -= 3
-                        sq = squares[pos]
-                        if sq == 'ch3':
-                            card = next(cc)
-                            if card is None:
-                                visits[sq] += 1
-                            else:
-                                pos = squares.index(card)
-                                visits[card] += 1
-                    else:
-                        pos = squares.index(card)
-                        visits[card] += 1
-            elif sq == 'g2j':
-                pos = squares.index('jail')
-                visits['jail'] += 1
-            else:
-                visits[sq] += 1 
-    visit_items = visits.items()
-    visit_items = sorted(visit_items, key=lambda x: x[1], reverse=True)
+        pos = (pos + sum(roll)) % 40
+        sq = squares[pos]
+        if sq.startswith('cc'):
+            pos = community_chest(sq, pos)
+        elif sq.startswith('ch'):
+            pos = chance(sq, pos)
+        elif sq == 'g2j':
+            pos = squares.index('jail')
+            visits['jail'] += 1
+        else:
+            visits[sq] += 1 
+    visit_items = sorted(visits.items(), key=lambda x: x[1], reverse=True)
     return ''.join([str(squares.index(sq[0])) for sq in visit_items[:3]])
     
 if __name__ == "__main__":
