660431a5da2f7ee35174a4b33e6a8d4268e881a1
Tim Konick <konick781@gmail.com>
2016-04-08 14:48:18 -0400

    Rm map of float add Py3 compat

--- py_solutions_61-70/Euler_69.py
+++ py_solutions_61-70/Euler_69.py
@@ -1,23 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 # Euler's Totient Function 
+
+from __future__ import print_function, division
 
 import math
 import timeit
 
 
-start = timeit.default_timer()
+try:
+    range = xrange
+except NameError:
+    pass
 
 def is_prime(x):
     if x == 2:
         return True
     if x % 2 == 0 or x == 1:
         return False
-    for i in xrange(3, int(math.sqrt(x)) + 1, 2):
+    for i in range(3, int(math.sqrt(x)) + 1, 2):
         if x % i == 0:
             return False
     return True
 
 def build_primes_list(limit):
-    return map(float, filter(is_prime, xrange(2, limit)))
+    return list(filter(is_prime, range(2, limit)))
 
 def calculate_phi(phi, primes):
     base = phi
@@ -30,7 +38,7 @@ def euler_69():
     high, high_phi = 0, 0
     limit = 1000001
     primes_list = build_primes_list(int(math.sqrt(limit)) + 1)
-    for i in xrange(2, limit):
+    for i in range(2, limit):
         prime_factor = int(math.sqrt(i)) 
         phi = calculate_phi(i, primes_list[:prime_factor])
         current = i / phi
@@ -38,7 +46,8 @@ def euler_69():
             high_phi, high = (current, i)
     return high
 
-print "Answer: %s" % euler_69()
-stop = timeit.default_timer()
-print "Time: %f" % (stop - start)
-
+if __name__ == '__main__':
+    start = timeit.default_timer()
+    print('Answer: {}'.format(euler_69()))
+    stop = timeit.default_timer()
+    print('Time: {0:9.5f}'.format(stop - start))
