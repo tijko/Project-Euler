0415575a5a41b254a59fbc13b17e21e8b9868bf0
Tim Konick <konick781@gmail.com>
2016-05-05 22:14:27 -0400

    Comments and helper func

--- py_solutions_91-100/Euler_91.py
+++ py_solutions_91-100/Euler_91.py
@@ -25,6 +25,14 @@ except NameError:
     pass
 
 
+def find_side_length(point, idx, sides_shared):
+    # point is @ zero increment shared side
+    if point[idx] == 0:
+        return point[not idx], sides_shared + 1
+    return sqrt(point[idx]**2 + point[not idx]**2), sides_shared
+
+side = lambda p1, p2, idx: abs(p1[idx] - p2[idx])
+
 def euler_91(dimension):
     count = 0
     max_x = max_y = dimension
@@ -35,34 +43,20 @@ def euler_91(dimension):
         # check if point origin is aligning the other two points to zero
         if triangle[0] == origin or p1[0] == p2[0] == 0 or p1[1] == p2[1] == 0:
             continue
-        sides_shared = 0
-        # point is @ zero inc shared side
         # if there are 2 or more shared sides it must be a right angle.
-        if p1[0] == 0:
-            sides_shared += 1
-            side1 = p1[1]
-        else:
-            # find the side length by creating a rectangle
-            # and using the distance between points to calculate
-            # the side as the hypotenuse.
-            side1 = sqrt(p1[0]**2 + p1[1]**2)
-        if p2[1] == 0:
-            sides_shared += 1
-            side2 = p2[0]
-        else:
-            side2 = sqrt(p2[0]**2 + p2[1]**2)
+        sides_shared = 0
+        side1, sides_shared = find_side_length(p1, 0, sides_shared)
+        side2, sides_shared = find_side_length(p2, 1, sides_shared)
         if p1[0] == p2[0] or p1[1] == p2[1]:
             sides_shared += 1
-            if p1[0] == p2[0]:
-                side3 = abs(p1[1] - p2[1])
-            else:
-                side3 = abs(p1[0] - p2[0])
+            side3 = side(p1, p2, 1) if p1[0] == p2[0] else side(p1, p2, 0)
         else:
             side3 = sqrt(abs(p1[0] - p2[0])**2 + abs(p1[1] - p2[1])**2) 
         if sides_shared >= 2:
             count += 1
             continue
         a, b, c = sorted([side1, side2, side3])
+        # this was giving a round-off error
         if isclose(sqrt(a**2 + b**2), c):
             count += 1
     return count
