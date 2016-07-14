9a85ea1b8dbeb36ee7a1d3e1f65c85437a581a9a
Tim <konick781@gmail.com>
2016-07-14 17:31:07 -0400

    C solution #28

---
+++ c_solutions_21-30/Euler_28.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+
+long spiral_total(int spiral_len)
+{
+    long total = 0;
+
+    for (int i=1; i < spiral_len + 1; i+=2)
+        total += (i * i);
+
+    return total;
+}        
+
+long grid(int step, int start_axis, int end_axis)
+{
+    long total = step;
+
+    for (int i=start_axis; step + i < end_axis; i+=8) {
+        step += i;
+        total += step;
+    }
+
+    return total;
+}
+
+int main(int argc, char *argv[])
+{
+    int diagonal_pos = 3;
+
+    long total = spiral_total(1001);
+    int end_axis = 1001994;
+
+    for (int i=10; i < 15; i+=2) {
+        total += grid(diagonal_pos, i, end_axis);
+        diagonal_pos += 2;
+        end_axis += 2;
+    }
+
+    printf("Answer: %ld\n", total);
+
+    return 0;
+}
