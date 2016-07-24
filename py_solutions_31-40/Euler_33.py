92ee6b92160e0dc83220b1b90ee493ab0826aa13
Tim <konick781@gmail.com>
2016-07-23 23:26:47 -0400

    Re-write of py solution #33

--- py_solutions_31-40/Euler_33.py
+++ py_solutions_31-40/Euler_33.py
@@ -8,31 +8,45 @@
 import timeit
 import itertools
 
+from functools import reduce
+from operator import mul, truediv
+
+try:
+    range = xrange
+except NameError:
+    pass
 
 start = timeit.default_timer()
+
 
 def euler_33():
     numerators = list()
     denominators = list()
-    product = lambda x, y: x * y
-    to_string = lambda x, y: str(x).replace(y, '')
-    fractions = (f for f in itertools.permutations(xrange(10, 100), 2) 
-                 if f[0] < f[1] and not any(i % 10 == 0 or i % 11 == 0 for i in f))
-    for f in fractions:
-        n_cpm = {d for d in str(f[0])}
-        d_cpm = {d for d in str(f[1])}
-        if n_cpm.intersection(d_cpm):
-            shared_d = list(n_cpm.intersection(d_cpm))
-            new_n = to_string(f[0], shared_d[0])
-            new_d = to_string(f[1], shared_d[0])
-            if float(new_n) / int(new_d) == float(f[0]) / f[1]:
-                numerators.append(f[0])
-                denominators.append(f[1])
-    fin_n = reduce(product, numerators)
-    fin_d = reduce(product, denominators)
-    n_lcms = [i for i in xrange(2, fin_n + 1) if fin_d % i == 0 and fin_n % i == 0]
-    return fin_d / n_lcms[-1]    
+    fractions = (f for f in itertools.combinations(range(10, 100), 2)
+                 if not any(i % 10 == 0 or i % 11 == 0 for i in f))
+    floordiv = lambda v: v // 10
+    mod = lambda v: v % 10
+    for fraction in fractions:
+        numerator, denominator = fraction
+        if mod(numerator) == mod(denominator):
+            ops = (floordiv, floordiv)
+        elif mod(numerator) == floordiv(denominator):
+            ops = (floordiv, mod)
+        elif floordiv(numerator) == mod(denominator):
+            ops = (mod, floordiv)
+        elif floordiv(numerator) == floordiv(denominator):
+            ops = (mod, mod)
+        else: continue
+        numerator, denominator = ops[0](numerator), ops[1](denominator)
+        if truediv(numerator, denominator) == reduce(truediv, fraction):
+            numerators.append(fraction[0])
+            denominators.append(fraction[1])
+    final_numerator = reduce(mul, numerators)
+    final_denominator = reduce(mul, denominators)
+    n_lcms = [i for i in range(2, final_numerator + 1) if 
+              final_denominator % i == 0 and final_numerator % i == 0]
+    return final_denominator / n_lcms[-1]    
 
-print "Answer: %s" % euler_33()
+print('Answer: {}'.format(euler_33()))
 stop = timeit.default_timer()
-print "Time: %f" % (stop - start)
+print('Time: {0:9.5f}'.format(stop - start))
