2778de0556fda49315daa2716a315eee32a7e0b0
Tim <konick781@gmail.com>
2016-07-23 23:26:18 -0400

    Untracked change

--- py_solutions_31-40/Euler_32.py
+++ py_solutions_31-40/Euler_32.py
@@ -5,17 +5,22 @@ import timeit
 import itertools
 
 
+try:
+    range = xrange
+except NameError:
+    pass
+
 start = timeit.default_timer()
 
 def euler_32():
     pandigitals = set()
     pandigital = {'1', '2', '3', '4', '5', '6', '7', '8', '9'}
-    for x, y in itertools.product(xrange(2, 100), xrange(100, 5000)):
+    for x, y in itertools.product(range(2, 100), range(100, 5000)):
         digi_str = ''.join(map(str, [x * y, x, y]))
         if len(digi_str) == 9 and {i for i in digi_str} == pandigital:
             pandigitals.add(x * y)
     return sum(pandigitals)
 
-print "Answer: %s" % euler_32()
+print('Answer: {}'.format(euler_32()))
 stop = timeit.default_timer()
-print "Time: %f" % (stop - start)
+print('Time: {0:9.5f}'.format(stop - start))
