e7fddda0068cd35fd3f16ea8335cbb9be3dc716b
Tim <konick781@gmail.com>
2016-07-28 04:23:11 -0400

    Rewrite

--- c_solutions_31-40/Euler_32.c
+++ c_solutions_31-40/Euler_32.c
@@ -1,71 +1,59 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdbool.h>
+#include <stdlib.h>
 
+#define MAX 13 
 
-int pandigits[9];
 int digits[5000];
+char pandigital[10];
+char *pandigital_string = "123456789";
 
-int length;
-int found;
 
-void peel(int *set)
+int total(int found)
 {
-    for (int i=0; i < 3; i++) {
-        int num = set[i];
-        while (num > 0) {
-            int current_digit = num % 10;
-            num /= 10;
-            length++;
-            pandigits[current_digit - 1] = 1;
-        }            
-    }
+    int sum = 0;
+
+    for (int i=0; i < found; i++)
+        sum += digits[i];
+    return sum;
 }
 
-bool is_pandigital(void)
+int is_pandigital(char *prime_string)
 {
-    int nine = 0;
+    memset(pandigital, '0', 9);
 
-    for (int i=0; i < 9; i++)
-        nine += pandigits[i];
+    for (int i=0; i < strlen(prime_string); i++)
+        pandigital[(prime_string[i] - '0') - 1] = prime_string[i];
+    pandigital[9] = '\0';
 
-    if (nine == 9) 
-        return true;
-
-    return false;
+    return strcmp(pandigital, pandigital_string) == 0 ? 1 : 0;
 }
 
-bool unique(int can)
+int unique(int can, int found)
 {
     for (int i=0; i < found; i++)
-        if (digits[i] == can) return false;
-    return true;
+        if (digits[i] == can) return 0;
+    return 1;
 }
 
 int main(int argc, char *argv[])
 {
-    int total = 0;
-    found = 0;
+    int found = 0;
 
-    memset(digits, 0, sizeof(int) * 5000);
+    char *prime_string = malloc(sizeof(char) * MAX + 1);
 
     for (int i=2; i < 100; i++) {
         for (int j=100; j < 5000; j++) {
-            memset(pandigits, 0, sizeof(int) * 9);
-            length = 0;
-            int set[3] = {i * j, i, j};
-            peel(set);
-            if (length > 9) continue;
-            if (is_pandigital()) 
-                if (unique(i*j))
-                    digits[found++] = i * j;
+            int product = i * j;
+            snprintf(prime_string, MAX, "%d%d%d", product, i, j);
+            if (strlen(prime_string) != 9 || strchr(prime_string, '0')) continue;
+            if (is_pandigital(prime_string) && unique(product, found)) 
+                digits[found++] = product;
         }
     }
-    
-    for (int i=0; i < found; i++)
-        total += digits[i];
+    printf("Answer: %d\n", total(found));
 
-    printf("%d\n", total);
+    free(prime_string);
 
     return 0;
 }
