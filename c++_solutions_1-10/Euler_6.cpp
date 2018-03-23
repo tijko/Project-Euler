13d8135be9c2baa6eedb8c4d5d57c6d5ad8ccddc
Tim Konick <konick781@gmail.com>
2018-03-22 21:51:26 -0400

    Rm unnecessary functions (e.g. accumulate) speed cut in half

--- c++_solutions_1-10/Euler_6.cpp
+++ c++_solutions_1-10/Euler_6.cpp
@@ -1,29 +1,22 @@
 #include "../euler_util.h"
 
 #include <vector>
-#include <numeric>
 #include <iostream>
 
-
-int sum(int total, int element)
-{
-    return total + element;
-}
 
 int main(int argc, char *argv[])
 {
     float start = timeit();
 
     long squares_sum = 0;
-    std::vector<int> sum_square;
+    long sum_square = 0;
 
     for (int i=1; i <= 100; i++) {
         squares_sum += i * i;
-        sum_square.push_back(i);
+        sum_square += i;
     }
 
-    long sum_total = pow(std::accumulate(sum_square.begin(), 
-                                         sum_square.end(), 0, sum), 2);
+    long sum_total = pow(sum_square, 2);
     long answer = sum_total - squares_sum;
 
     float stop = timeit();
