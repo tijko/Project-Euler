e150666c1fc8b39b0887ff0a1cb54cddd7804206
Tim Konick <konick781@gmail.com>
2018-07-13 00:35:20 -0400

    Haskell Solution #9

--- haskell_1-10/Euler_9.hs
+++ haskell_1-10/Euler_9.hs
@@ -2,5 +2,13 @@ module Main where
 
 -- only pythagorean triplet that a + b + c == 1000
  
+triplet :: [(Float, Float)] -> Float
+triple [] = 0
+triplet (pair:xs)
+    | (fst pair) + (snd pair) + z == 1000 = (fst pair) * (snd pair) * z
+    | otherwise = triplet xs
+  where
+    z = sqrt((fst pair)^2 + (snd pair)^2)
 main =
+    print(triplet [ (x, y) | x <- [1..1000], y <- [x+1..1000] ])
     
