b79d7e0822a60b6fdc12e568d7ed61f0f5878b81
tim <konick781@gmail.com>
2018-10-14 17:29:00 -0400

    Speed up exponentially

--- haskell_1-10/Euler_3.hs
+++ haskell_1-10/Euler_3.hs
@@ -2,23 +2,25 @@ module Main where
 
 
 is_factor :: Int -> [Int] -> Bool
+is_factor _ [] = True
 is_factor x y
-    | length y == 0       = True
     | x `mod` (y!!0) == 0 = False
     | otherwise           = is_factor x (tail y)
 
 
 prime_factor :: Int -> [Int] -> Int
+prime_factor _ [] = -1
 prime_factor n f 
-    | length f == 0          = -1
     | n `mod` (f!!0) == 0 && 
       is_prime (f!!0)        = f!!0
     | otherwise              = prime_factor n (tail f)
 
 is_prime :: Int -> Bool
+is_prime 2 = True
+is_prime 3 = True
 is_prime n  
-    | n == 2 || n == 3 = True
-    | n < 2 || n `mod` 2 == 0 = False
+    | n < 2          = False
+    | n `mod` 2 == 0 = False
     | otherwise = is_factor n divs
   where
     s = ceiling ( sqrt(fromIntegral n) ) + 1
