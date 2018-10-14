04d4aaed3c747263fad13941b2e72694e856d888
tim <konick781@gmail.com>
2018-10-14 17:27:52 -0400

    Minor style edit

--- haskell_1-10/Euler_1.hs
+++ haskell_1-10/Euler_1.hs
@@ -2,8 +2,9 @@ module Main where
 
 five_or_three :: Int -> Int
 five_or_three n 
-    | n `mod` 5 == 0 || mod n 3 == 0 = n
-    | otherwise                      = 0
+    | n `mod` 5 == 0 = n
+    | n `mod` 3 == 0 = n
+    | otherwise      = 0
 
 main = print(sum (map five_or_three [1..999]))
 
