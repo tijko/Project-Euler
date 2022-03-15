37d64f1487a2595adc2c6034c66846aecc675c78
tim konick <tkonick@nedelta.com>
2022-03-14 22:14:25 -0400

    First .NET solution

---
+++ NET_solutions_1-10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_solutions_1_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UInt64 total = 0;
+            for (UInt64 i=0; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                    total = total + i;
+            }
+
+            Console.WriteLine("Answer: {0}\n", (total));
+        }
+    }
+}
