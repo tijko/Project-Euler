cc0df0a473d04086a143a7348a11c75312c1fe50
Tim Konick <konick781@gmail.com>
2022-11-08 18:28:33 -0500

    Groovy Euler First Problem

---
+++ groovy_solutions_1-10/Euler_1.groovy
@@ -0,0 +1,24 @@
+#!/usr/bin/env groovy
+
+import groovy.time.TimeCategory
+import groovy.time.TimeDuration
+
+class Euler_1 {
+
+    static void main(String[] args) {
+
+        Date start = new Date()
+        int answer = 0;
+        int limit = 1000;
+        for (int i=0; i < limit; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                answer += i
+            }
+        }
+
+        Date end = new Date()
+        println("Answer: " + answer)
+        TimeDuration time = TimeCategory.minus(end, start)
+        println("Time: " + time)
+    }
+}
