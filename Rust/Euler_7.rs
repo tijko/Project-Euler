95104d6448b15157518f7eaba104af4d568440c0
Tim Konick <konick781@gmail.com>
2023-09-28 13:16:22 -0400

    Primetime

---
+++ Rust/Euler_7.rs
@@ -0,0 +1,43 @@
+// What is the 10001st prime number?
+
+use std::time::Instant;
+
+fn is_prime(n:u64) -> bool {
+    if n == 2 {
+        return true;
+    }
+
+    if n < 2 || n % 2 == 0 {
+        return false;
+    }
+
+    let mut range:u64 = (n as f64).sqrt() as u64;
+    range += 1;
+    for i in 3..range {
+        if n % i == 0 {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+fn main() {
+    let timer = Instant::now();
+    let mut count:u64 = 1;
+    let mut current:u64 = 2;
+
+    let answer = loop {
+        if count == 10001 {
+            break current;
+        }
+
+        current += 1;
+        if is_prime(current) {
+            count += 1;
+        }
+    };
+
+    println!("Answer: {}", answer);
+    println!("Time:  {:?}", timer.elapsed());
+}
