bd554f34c0dfbe2d5fb9a0d0b777403f7362e6a9
Tim Konick <konick781@gmail.com>
2023-09-30 03:02:17 -0400

    Rust #11

---
+++ Rust/Rust_solutions_11-20/Euler_11.rs
@@ -0,0 +1,9 @@
+// What is the greatest product of four adjacent numbers in any direction 
+// (up, down, left, right, or diagonally) in the 20x20 grid? 
+
+use std::time::Instant;
+
+fn main() {
+    let timer = Instant::now();
+    println!("Time:  {:?}", timer.elapsed());
+}
