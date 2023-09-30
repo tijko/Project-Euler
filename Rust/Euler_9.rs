87fbf3226256cf51b2868716d87f9833974b1f37
Tim Konick <konick781@gmail.com>
2023-09-30 02:35:00 -0400

    Fixes floating-point precision error

--- Rust/Euler_9.rs
+++ Rust/Euler_9.rs
@@ -6,15 +6,14 @@ use std::time::Instant;
 fn main() {
     let timer = Instant::now();
     let mut answer = 0;
-
     for x in 1..1000 as u64 {
         for y in x+1..1000 as u64 {
-            let tmp = (x*x + y*y) as f64;
-            let zi = tmp.sqrt();
-            let z = zi as u64;
-            if z == zi && x + y + z == 1000 {
-                println!("{} + {} + {}", x, y, z);
-                answer = x * y * z;
+            let z = ((x*x + y*y) as f64).sqrt();
+            if z != (z as u32) as f64 {
+                continue;
+            }
+            if x + y + z as u64 == 1000 {
+                answer = x * y * z as u64;
                 break;
             }
             if answer != 0 {
