908d04af160028022bf6fe468d11512b20b5976f
Tim Konick <konick781@gmail.com>
2023-10-03 22:58:59 -0400

    Fix Haskell #1

--- Haskell/haskell_1-10/Euler_1.hs
+++ Haskell/haskell_1-10/Euler_1.hs
@@ -1,4 +1,8 @@
+-- Find sum of all multiples of 3 and 5 under 1000
+
 module Main where
+import Text.Printf
+import System.Clock
 
 five_or_three :: Int -> Int
 five_or_three n 
@@ -6,6 +10,16 @@ five_or_three n
     | n `mod` 3 == 0 = n
     | otherwise      = 0
 
-main = print(sum (map five_or_three [1..999]))
+getTotalTime :: TimeSpec -> Float
+getTotalTime t = fromIntegral (sec t) + (fromIntegral(nsec t) / 10^9)
+
+main :: IO ()
+main = 
+  do 
+    start <- getTime Monotonic
+    printf "Answer: %d\n" (sum (map five_or_three [1..999]))
+    stop <- getTime Monotonic
+    printf "Time:   %.6f\n" (getTotalTime(stop) - getTotalTime(start))
+
 
                     
