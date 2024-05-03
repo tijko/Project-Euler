aa568721f85c67f06ec50cbd45ad715778f467d6
Tim Konick <konick781@gmail.com>
2024-05-02 23:11:39 -0400

    Use OptionalInt with IntStream

--- Java/java_solutions_1-10/Euler_1.java
+++ Java/java_solutions_1-10/Euler_1.java
@@ -1,28 +1,23 @@
-/* 
+/*
 /   Project Euler problem #1 Multiples of 3 and 5
 /
 /   Find the sum of all the multiples of 3 or 5 below 1000
 /
 */
-import java.util.stream.*;
-import java.util.List;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
 
 public class Euler_1
 {
     public static int factors(int limit)
     {
-        int sum = 0;
-        // IntStream 'boxed' method to convert from primitive [int] type
-        List<Integer> range = IntStream.range(1, limit)
-                                       .boxed()
-                                       .collect(Collectors.toList());
-        for (int i:range) {
-            if (i % 5 == 0 || i % 3 == 0)
-                sum += i;
-        }
-
-        return sum;
-    }           
+        // Use OptionalInt over IntStream with a map-reduce like
+        OptionalInt sum = IntStream.range(1, limit)
+                                   .filter(x -> x % 3 == 0 ||
+                                                x % 5 == 0)
+                                   .reduce(Integer::sum);
+        return sum.getAsInt();
+    }
 
     public static void main(String[] args)
     {
@@ -31,7 +26,7 @@ public class Euler_1
         int factor_sum = factors(1000);
 
         long stop = System.nanoTime();
-        System.out.println("Answer: " + factor_sum); 
+        System.out.println("Answer: " + factor_sum);
         System.out.printf("Time: %.7f\n", ((float) stop - (float) start) / 10000000);
     }
-}    
+}
