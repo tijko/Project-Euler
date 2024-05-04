254a8a6a3cdc1f288312152e97c43a942bb17b57
Tim Konick <konick781@gmail.com>
2024-05-04 00:51:09 -0400

    Because Recursive it is

--- Java/java_solutions_1-10/Euler_2.java
+++ Java/java_solutions_1-10/Euler_2.java
@@ -4,29 +4,30 @@
 /   Find the sum of the even fibonacci terms <= 4 million
 /
 */
+import java.util.stream.IntStream;
+import java.util.OptionalInt;
 
 public class Euler_2
 {
+    public static long fibonacci(long fnum, long idx, int limit, long total) {
+        if (fnum >= limit) {
+            return total;
+        }
+
+        if (fnum % 2 == 0) {
+            total += fnum;
+        }
+
+        return fibonacci(fnum + idx, fnum, limit, total);
+    }
+
     public static void main(String[] args)
     {
         long start = System.nanoTime();
-        int sumFibonacci = 0;
-        int prevTerm = 0;
-        int tmp = 0;
-        int currentTerm = 1;
-
-        while (currentTerm <= 4000000) {
-
-            if (currentTerm % 2 == 0) 
-                sumFibonacci += currentTerm;
-
-            tmp = currentTerm;
-            currentTerm = currentTerm + prevTerm;
-            prevTerm = tmp;
-        }
+        long sumFibonacci = fibonacci(3, 2, 4000000, 0);
         long stop = System.nanoTime();
         System.out.println("Answer: " + sumFibonacci);
-        System.out.printf("Time: %.7\n", ((float) stop - start);
+        System.out.printf("Time: %.7f\n", ((float) stop - start));
     }
 }
             
