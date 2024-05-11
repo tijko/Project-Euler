f66f2870d95fce38f82eff1af09c55de15914c2e
Tim Konick <konick781@gmail.com>
2024-05-10 22:59:15 -0400

    Euler #9 Java Re-Factor

--- Java/java_solutions_1-10/Euler_9.java
+++ Java/java_solutions_1-10/Euler_9.java
@@ -9,22 +9,36 @@ import static java.lang.Math.sqrt;
 
 public class Euler_9
 {
-    static int triplet = 0;
-
-    public static void checkTriplet(int a, int b)
+    public static long findTriplet(int a, int b)
     {
         int c = (a * a) + (b * b);
-        if (sqrt(c) + a + b == 1000) 
-            triplet = (int) sqrt(c) * a * b;
+        if (sqrt(c) + a + b == 1000) { 
+            long triplet = (int) sqrt(c) * a * b;
+            return triplet;
+        }
+        return 0;
     }
-
 
     public static void main(String[] args)
     {
-        for (int a = 1; a < 1000; a++) 
-            for (int b = 1; b < 1000; b++) 
-                checkTriplet(a, b);
+        long start = System.nanoTime();
+        long triplet = 0;
+        long current = 0;
+        for (int a = 1; a < 1000; a++) { 
+            for (int b = 1; b < 1000; b++) {
+                current = findTriplet(a, b);
+                if (current > 0) {
+                    triplet = current;
+                    break;
+                }
+            }
+            if (triplet != 0) {
+                break;
+            }
+        }
 
         System.out.println("Answer: " + triplet);
+        long stop = System.nanoTime();
+        System.out.printf("Time: %.4f\n", ((float) stop - start) / 1_000_000_000);
     }
 }   
