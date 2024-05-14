891503151604e3ffe332ce5b5cb3feda27a98489
Tim Konick <konick781@gmail.com>
2024-05-13 23:21:04 -0400

    Remove Trailing Space

--- Java/java_solutions_11-20/Euler_14.java
+++ Java/java_solutions_11-20/Euler_14.java
@@ -22,12 +22,12 @@ public class Euler_14
         return sequenceCount;
     }
 
-    public static long findLongestChain(long limit, 
-                                       long candidate, 
-                                       long longestChain, 
+    public static long findLongestChain(long limit,
+                                       long candidate,
+                                       long longestChain,
                                        long longestChainStart) {
         if (candidate > limit) {
-            return longestChainStart; 
+            return longestChainStart;
         }
 
         long currentChain = collatzSequence(candidate);
@@ -36,7 +36,7 @@ public class Euler_14
             longestChainStart = candidate;
         }
 
-        return findLongestChain(limit, candidate + 1, longestChain, 
+        return findLongestChain(limit, candidate + 1, longestChain,
                                                       longestChainStart);
     }
 
@@ -45,7 +45,7 @@ public class Euler_14
         long start = System.nanoTime();
         long answer = findLongestChain(1000000, 0, 0, 0);
         long stop = System.nanoTime();
-        System.out.println("Answer: " + answer); 
-        System.out.printf("Time: %.4f\n", ((float) stop - start) / 1_000_000_000);            
+        System.out.println("Answer: " + answer);
+        System.out.printf("Time: %.4f\n", ((float) stop - start) / 1_000_000_000);
     }
 }
