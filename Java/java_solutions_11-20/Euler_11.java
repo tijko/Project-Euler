0ab92400aaf6bc7ab8a6d661efeec2c929508ada
Tim Konick <konick781@gmail.com>
2024-05-13 23:21:51 -0400

    Remove Trailing Space

--- Java/java_solutions_11-20/Euler_11.java
+++ Java/java_solutions_11-20/Euler_11.java
@@ -10,7 +10,7 @@ public class Euler_11
 {
     public static int[][] grid = new int[20][20];
 
-    public static String gridString = 
+    public static String gridString =
     "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08 "+
     "49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00 "+
     "81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65 "+
@@ -42,7 +42,7 @@ public class Euler_11
             int rowLen = rowSplit.length;
             int[] rowInt = new int[rowLen];
 
-            for (int j = 0; j < rowLen; j++) 
+            for (int j = 0; j < rowLen; j++)
                 rowInt[j] = Integer.parseInt(rowSplit[j]);
 
             grid[i] = rowInt;
@@ -51,9 +51,9 @@ public class Euler_11
 
     public static int moves[][]       = { {0, 3}, {3, 0}, {3, 3}, {3, -3} };
     public static int adjacents[][][] = {
-                                          {{0, 0}, {0, 1}, {0, 2}, {0, 3}}, 
-                                          {{0, 0}, {1, 0}, {2, 0}, {3, 0}}, 
-                                          {{0, 0}, {1, 1}, {2, 2}, {3, 3}}, 
+                                          {{0, 0}, {0, 1}, {0, 2}, {0, 3}},
+                                          {{0, 0}, {1, 0}, {2, 0}, {3, 0}},
+                                          {{0, 0}, {1, 1}, {2, 2}, {3, 3}},
                                           {{0, 0}, {1, -1}, {2, -2}, {3, -3}}
                                         };
 
@@ -71,12 +71,12 @@ public class Euler_11
     public static long checkAdjacentNodes(int row, int col, long greatestProduct)
     {
         for (int i = 0; i < moves.length; i++) {
-            if (row + moves[i][0] < gridLength && 
+            if (row + moves[i][0] < gridLength &&
                 col + moves[i][1] < grid[row].length &&
                 col + moves[i][1] >= 0) {
                 long nodeProduct = calculateNodeTotal(adjacents[i], row, col);
-                greatestProduct = greatestProduct > nodeProduct ? 
-                                   greatestProduct : nodeProduct; 
+                greatestProduct = greatestProduct > nodeProduct ?
+                                   greatestProduct : nodeProduct;
             }
         }
 
