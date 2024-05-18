36c1569b48121273702a2d25fd913e99ab1f6143
Tim Konick <konick781@gmail.com>
2024-05-17 22:38:25 -0400

    Re-arrange Prime Finder

--- Python/py_solutions_21-30/Euler_26.py
+++ Python/py_solutions_21-30/Euler_26.py
@@ -10,24 +10,18 @@ try:
 except NameError:
     pass
 
-start = timeit.default_timer()
-
 def is_prime(x):
-    if x == 2:
-        return True
-    if x % 2 == 0 or x == 1:
-        return False
-    for i in range(3, int(math.sqrt(x) + 1), 2):
-        if x % i == 0:
-            return False
-    return True
-
+    if x == 2: return True
+    if x % 2 == 0 or x < 2: return False
+    return all(x % i != 0 for i in range(3, int(math.sqrt(x)) + 1, 2))
 
 def euler_26():
     primes_to_onethousand = list(filter(is_prime, range(1, 1000)))
     return primes_to_onethousand[len(primes_to_onethousand) - 3]
 
 
-print("Answer: {}".format(euler_26()))
-stop = timeit.default_timer()
-print("Time: {0:9.5f}".format(stop - start))
+if __name__ == '__main__':
+    start = timeit.default_timer()
+    print("Answer: {}".format(euler_26()))
+    stop = timeit.default_timer()
+    print("Time: {0:9.5f}".format(stop - start))
