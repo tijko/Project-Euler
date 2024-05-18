b8bb8bc54f4d9bf23655f385db7a8fb66f5af9f6
Tim Konick <konick781@gmail.com>
2024-05-18 19:55:35 -0400

    Whitespace

--- Python/py_solutions_81-90/Euler_87.py
+++ Python/py_solutions_81-90/Euler_87.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 
 '''
-The smallest number expressible as the sum of a prime square, prime cube, and 
-prime fourth power is 28. In fact, there are exactly four numbers below fifty 
+The smallest number expressible as the sum of a prime square, prime cube, and
+prime fourth power is 28. In fact, there are exactly four numbers below fifty
 that can be expressed in such a way:
 
 28 = 22 + 23 + 24
@@ -11,7 +11,7 @@ that can be expressed in such a way:
 49 = 52 + 23 + 24
 47 = 22 + 33 + 24
 
-How many numbers below fifty million can be expressed as the sum of a prime 
+How many numbers below fifty million can be expressed as the sum of a prime
 square, prime cube, and prime fourth power?
 '''
 
