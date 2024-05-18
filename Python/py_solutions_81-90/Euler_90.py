7b7d53f8a9511330fefdaa588a71ca64a4b87618
Tim Konick <konick781@gmail.com>
2024-05-18 19:54:15 -0400

    Whitespace

--- Python/py_solutions_81-90/Euler_90.py
+++ Python/py_solutions_81-90/Euler_90.py
@@ -2,33 +2,33 @@
 # -*- coding: utf-8 -*-
 
 '''
-Each of the six faces on a cube has a different digit (0 to 9) written on it; 
-the same is done to a second cube. By placing the two cubes side-by-side in 
+Each of the six faces on a cube has a different digit (0 to 9) written on it;
+the same is done to a second cube. By placing the two cubes side-by-side in
 different positions we can form a variety of 2-digit numbers.
 
-In fact, by carefully choosing the digits on both cubes it is possible to 
-display all of the square numbers below one-hundred: 01, 04, 09, 16, 25, 36, 49, 
+In fact, by carefully choosing the digits on both cubes it is possible to
+display all of the square numbers below one-hundred: 01, 04, 09, 16, 25, 36, 49,
 64, and 81.
 
-For example, one way this can be achieved is by placing {0, 5, 6, 7, 8, 9} on 
+For example, one way this can be achieved is by placing {0, 5, 6, 7, 8, 9} on
 one cube and {1, 2, 3, 4, 8, 9} on the other cube.
 
-However, for this problem we shall allow the 6 or 9 to be turned upside-down so 
-that an arrangement like {0, 5, 6, 7, 8, 9} and {1, 2, 3, 4, 6, 7} allows for 
-all nine square numbers to be displayed; otherwise it would be impossible to 
+However, for this problem we shall allow the 6 or 9 to be turned upside-down so
+that an arrangement like {0, 5, 6, 7, 8, 9} and {1, 2, 3, 4, 6, 7} allows for
+all nine square numbers to be displayed; otherwise it would be impossible to
 obtain 09.
 
-In determining a distinct arrangement we are interested in the digits on each 
+In determining a distinct arrangement we are interested in the digits on each
 cube, not the order.
 
 {1, 2, 3, 4, 5, 6} is equivalent to {3, 6, 4, 1, 2, 5}
 {1, 2, 3, 4, 5, 6} is distinct from {1, 2, 3, 4, 5, 9}
 
 But because we are allowing 6 and 9 to be reversed, the two distinct sets in the
-last example both represent the extended set {1, 2, 3, 4, 5, 6, 9} for the 
+last example both represent the extended set {1, 2, 3, 4, 5, 6, 9} for the
 purpose of forming 2-digit numbers.
 
-How many distinct arrangements of the two cubes allow for all of the square 
+How many distinct arrangements of the two cubes allow for all of the square
 numbers to be displayed?
 '''
 
@@ -52,10 +52,10 @@ def euler_90():
         s2_prod = {''.join(i) for i in product(s2, s1)}
         if squares & (s1_prod | s2_prod) == squares:
             count += 1
-    return count 
+    return count
 
 if __name__ == '__main__':
     start = timeit.default_timer()
     print('Answer: {}'.format(euler_90()))
     stop = timeit.default_timer()
-    print('Time: {0:9.5f}'.format(stop - start))    
+    print('Time: {0:9.5f}'.format(stop - start))
