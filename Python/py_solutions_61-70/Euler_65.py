7d84cd0cd4fe42f45e3b388f16fb1ba228239e29
Tim Konick <konick781@gmail.com>
2024-05-18 21:58:25 -0400

    Fixed spacing

--- Python/py_solutions_61-70/Euler_65.py
+++ Python/py_solutions_61-70/Euler_65.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# for the constant e find the sum of the 10000 convergents numerator 
+# for the constant e find the sum of the 10000 convergents numerator
 
 from __future__ import print_function
 
@@ -24,7 +24,7 @@ def euler_65():
     old_d = 3
     n = 11
     d = 4
-    convergents = list() 
+    convergents = list()
     for c in xrange(4, len(periods)):
         new_n = (n * periods[c]) + old_n
         new_d = (d * periods[c]) + old_d
@@ -36,6 +36,7 @@ def euler_65():
     total = sum([int(i) for i in str(convergents[95][0])])
     return total
 
-print("Answer: %s" % euler_65())
-stop = timeit.default_timer()
-print("Time: %f" % (stop - start))
+if __name__ == '__main__':
+    print("Answer: %s" % euler_65())
+    stop = timeit.default_timer()
+    print("Time: %f" % (stop - start))
